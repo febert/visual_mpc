7ffd381d1c5d2050652e22e410383906e31ace90
febert <frederik.ebert@mytum.de>
2017-02-28 19:58:41 -0800

    sc

--- tensorflow_data/hidden_state/latent_model_nomasks_kern9_4x4/conf.py
+++ tensorflow_data/hidden_state/latent_model_nomasks_kern9_4x4/conf.py
@@ -32,5 +32,6 @@ configuration = {
 'use_conv_low_dim_state':'',  # use low dimensional state computed by convolutions
 'train_latent_model':'',       # whether to add a loss for the latent space model to the objective
 'dna_size': 9,
-'4x4lowdim':''              # use 16 dimensional hidden state
+'4x4lowdim':'',              # use 16 dimensional hidden state
+'lt_state_factor': 1.0
 }
