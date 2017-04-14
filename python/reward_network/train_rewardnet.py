dde960ff2d69b69bd279079188d6c2100ff35dca
febert <frederik.ebert@mytum.de>
2017-04-13 19:00:15 -0700

    sc

--- python/reward_network/train_rewardnet.py
+++ python/reward_network/train_rewardnet.py
@@ -30,7 +30,7 @@ SUMMARY_INTERVAL = 40
 VAL_INTERVAL = 200
 
 # How often to save a model checkpoint
-SAVE_INTERVAL = 20  #00
+SAVE_INTERVAL = 2000
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('hyper', '', 'hyperparameters configuration file')
