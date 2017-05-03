cbe6c156eb4c1a80d693335b351afb90bda6a49c
febert <frederik.ebert@mytum.de>
2017-05-02 20:57:11 -0700

    sc

--- python/lsdc/utility/trajectory.py
+++ python/lsdc/utility/trajectory.py
@@ -18,10 +18,10 @@ class Trajectory(object):
         self.gtruth_images = None
 
         self.U = np.empty([self.T, 4])
-        self.X_full = np.empty([self.T, 3])
-        # self.Xdot_full = np.empty([self.T, 2])
+        self.X_full = np.empty([self.T, 2])
+        self.Xdot_full = np.empty([self.T, 2])
         self.Object_pos = np.empty((self.T, hyperparams['num_objects'], 7))
-        # self.X_Xdot_full = np.empty([self.T, 4])
+        self.X_Xdot_full = np.empty([self.T, 4])
 
         self.desig_pos = np.empty([self.T, 2])
         self.score = np.empty([self.T])
