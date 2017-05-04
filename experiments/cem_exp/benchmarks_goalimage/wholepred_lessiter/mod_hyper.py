481553cfb09df39cdd3249e306c6f770c8737df7
febert <frederik.ebert@mytum.de>
2017-05-03 18:31:31 -0700

    sc

--- experiments/cem_exp/benchmarks_goalimage/wholepred_lessiter/mod_hyper.py
+++ experiments/cem_exp/benchmarks_goalimage/wholepred_lessiter/mod_hyper.py
@@ -25,12 +25,12 @@ agent = {
     'use_goalimage':"",
     'start_confs': bench_dir + '/make_easy_goal_1e4/configs_easy_goal_1e4',
     'store_whole_pred':'',
-    # 'novideo':''
+    'novideo':''
 }
 
 config = {
     'save_data': True,
-    'traj_per_file': 200
+    'traj_per_file': 10
 }
 common = {
     'data_files_dir': current_dir + '/tfrecords/train',
