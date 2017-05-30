93696ae6f37957b54bb3b0f42d50fd4c20ec845a
Sawyer <manu.upadhyaya.42@gmail.com>
2017-05-30 13:21:21 -0700

    sc

--- python/video_prediction/sawyer/setup_predictor_sawyer.py
+++ python/video_prediction/sawyer/setup_predictor_sawyer.py
@@ -18,6 +18,7 @@ class Tower(object):
         per_gpu_actions = tf.slice(actions, [startidx, 0, 0], [nsmp_per_gpu, -1, -1])
         start_images = tf.slice(start_images, [startidx, 0, 0, 0, 0], [nsmp_per_gpu, -1, -1, -1, -1])
         start_states = tf.slice(start_states, [startidx, 0, 0], [nsmp_per_gpu, -1, -1])
+        pix_distrib = tf.slice(pix_distrib, [startidx, 0, 0], [nsmp_per_gpu, -1, -1])
 
         print 'startindex for gpu {0}: {1}'.format(gpu_id, startidx)
 
