340a188cec49a63fc8bff8fd15cdd4e0efb91082
Sawyer <manu.upadhyaya.42@gmail.com>
2017-06-09 10:36:53 -0700

    sc

--- experiments/cem_exp/benchmarks_sawyer/avoid_occlusions_firstplan/mod_hyper.py
+++ experiments/cem_exp/benchmarks_sawyer/avoid_occlusions_firstplan/mod_hyper.py
@@ -14,7 +14,7 @@ policy = {
     'iterations': 3,
     'load_goal_image':'make_easy_goal',
     'verbose':'',
-    'predictor_propagation': '',   # use the model get the designated pixel for the next step!
+    # 'predictor_propagation': '',   # use the model get the designated pixel for the next step!
     'action_cost_factor': 0,
     # 'reuse_mean_cov':'',
     # 'no_instant_gif':"",
