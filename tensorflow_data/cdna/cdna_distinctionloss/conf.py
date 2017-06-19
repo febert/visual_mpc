c5617a5b7a13f7aaef5131a6fe1b4a8cf314fe21
febert <frederik.ebert@mytum.de>
2017-06-19 11:15:34 -0700

    sc

--- tensorflow_data/cdna/cdna_distinctionloss/conf.py
+++ tensorflow_data/cdna/cdna_distinctionloss/conf.py
@@ -2,7 +2,7 @@ import os
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
 # tf record data location:
-DATA_DIR = '/'.join(str.split(current_dir, '/')[:-2]) + '/pushing_data/random_action_var10/train'
+DATA_DIR = '/'.join(str.split(current_dir, '/')[:-3]) + '/pushing_data/random_action_var10/train'
 
 # local output directory
 OUT_DIR = current_dir + '/modeldata'
