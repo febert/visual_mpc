141ab25747d5f7003a1dc71f12bb858b4bb2e83e
febert <frederik.ebert@mytum.de>
2017-08-22 16:41:18 -0700

    sc

--- tensorflow_data/clean_tensorflowdata.py
+++ tensorflow_data/clean_tensorflowdata.py
@@ -8,19 +8,22 @@ import pdb
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
 for path, subdirs, files in os.walk(current_dir):
-    # print path
-    # print subdirs
-    # print files
+    print path
+    print subdirs
+    print files
 
     iter_numbers = []
     for file in files:
 
         if "model" in file:
-            iter_num = re.match('.*?([0-9]+)$', file)
+            # iter_num = re.match('.*?([0-9]+)$', file)
+            iter_num = re.search(r'\d+', file).group()
             if iter_num == None:
                 continue
             else:
-                iter_numbers.append(int(iter_num.group(1)))
+                if int(iter_num) not in iter_numbers:
+                    iter_numbers.append(int(iter_num))
+
     if iter_numbers != []:
         max_iter = np.max(np.array(iter_numbers))
         iter_numbers.remove(max_iter)
@@ -29,9 +32,11 @@ for path, subdirs, files in os.walk(current_dir):
             print 'max iternumber found in {}:{}'.format(path, max_iter)
             print 'going to delete: {}'.format(iter_numbers)
 
+
             for n in iter_numbers:
                 try:
-                    os.remove(os.path.join(path,'model' +str(n)))
+                    os.remove(os.path.join(path, 'model' + str(n)) + '.data-00000-of-00001')
+                    os.remove(os.path.join(path,'model' +str(n)) +'.index')
                     os.remove(os.path.join(path,'model' +str(n)+'.meta'))
                 except:
                     print 'not able to remove'
