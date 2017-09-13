a4b81bbc91f504866eb0c34cf01e410064d030d8
febert <frederik.ebert@mytum.de>
2017-09-13 13:43:18 -0700

    sc

--- python_visual_mpc/video_prediction/basecls/prediction_model_basecls.py
+++ python_visual_mpc/video_prediction/basecls/prediction_model_basecls.py
@@ -1,20 +1,3 @@
-# Copyright 2016 The TensorFlow Authors All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Model architecture for predictive model, including CDNA, DNA, and STP."""
-
 import numpy as np
 import tensorflow as tf
 
@@ -23,22 +6,27 @@ from tensorflow.contrib.layers.python import layers as tf_layers
 from python_visual_mpc.video_prediction.lstm_ops12 import basic_conv_lstm_cell
 from python_visual_mpc.misc.zip_equal import zip_equal
 
-import pdb
+from utils.transformations import dna_transformation, cdna_transformation
 
-# Amount to use when lower bounding tensors
-RELU_SHIFT = 1e-12
+
+import pdb
 
 
 class Base_Prediction_Model(object):
 
     def __init__(self,
-                images=None,
-                actions=None,
-                states=None,
                 iter_num=-1.0,
-                pix_distrib1=None,
                 conf = None,
-                trafo_pix = True):
+                trafo_pix = True,
+                load_data = True,
+                mode=True):
+        """
+        :param iter_num:
+        :param conf:
+        :param trafo_pix: whether to transform  distributions of designated pixels
+        :param load_data:  whether to load data
+        :param mode:  whether to build train- or val-model
+        """
 
         self.cdna, self.stp, self.dna = False, False, False
         if self.conf['model'] == 'CDNA':
@@ -55,25 +43,35 @@ class Base_Prediction_Model(object):
         self.num_masks = conf['num_masks']
         self.context_frames = conf['context_frames']
 
-        if actions is None:
+        if not load_data:
             self.actions_pl = tf.placeholder(tf.float32, name='actions',
                                         shape=(conf['batch_size'], conf['sequence_length'], 4))
             actions = self.actions_pl
 
-        if states is None:
             self.states_pl = tf.placeholder(tf.float32, name='states',
                                        shape=(conf['batch_size'], conf['sequence_length'], 3))
             states = self.states_pl
 
-        if images is None:
             self.images_pl = tf.placeholder(tf.float32, name='images',
                                        shape=(conf['batch_size'], conf['sequence_length'], 64, 64, 3))
             images = self.images_pl
 
-        if pix_distrib1 is None:
             self.pix_distrib_pl = tf.placeholder(tf.float32, name='states',
                                             shape=(conf['batch_size'], conf['sequence_length'], 64, 64, 1))
+            pix_distrib1  = self.pix_distrib_pl
 
+        else:
+            if 'sawyer' in conf:
+                from python_visual_mpc.video_prediction.read_tf_record_sawyer12 import build_tfrecord_input
+            else:
+                from python_visual_mpc.video_prediction.read_tf_record import build_tfrecord_input
+
+            if mode == 'train':
+                images, actions, states = build_tfrecord_input(conf, training=True)
+            elif mode == 'val':
+                images, actions, states = build_tfrecord_input(conf, training=False)
+            else:
+                raise ValueError
 
         self.batch_size, self.img_height, self.img_width, self.color_channels = [int(i) for i in
                                                                                  images[0].get_shape()[0:4]]
@@ -116,13 +114,24 @@ class Base_Prediction_Model(object):
         states = [tf.squeeze(st) for st in states]
         images = tf.split(axis=1, num_or_size_splits=images.get_shape()[1], value=images)
         images = [tf.squeeze(img) for img in images]
-        pix_distrib1 = tf.split(axis=1, num_or_size_splits=pix_distrib1.get_shape()[1], value=pix_distrib1)
-        self.pix_distrib1= [tf.squeeze(pix) for pix in pix_distrib1]
+
+        if trafo_pix:
+            pix_distrib1 = tf.split(axis=1, num_or_size_splits=pix_distrib1.get_shape()[1], value=pix_distrib1)
+            self.pix_distrib1= [tf.squeeze(pix) for pix in pix_distrib1]
 
         self.actions = actions
         self.iter_num = iter_num
         self.conf = conf
         self.images = images
+
+        if mode == 'train':
+            with tf.variable_scope('model', reuse=None) as training_scope:
+                self.build()
+        elif mode == 'val':
+            with tf.variable_scope('val_model', reuse=None):
+                self.build()
+        else:
+            raise ValueError
 
 
     def random_shift(self, images, states, actions):
@@ -212,6 +221,7 @@ class Base_Prediction_Model(object):
                                                          prev_pix_distrib1, prev_pix_distrib2, reuse, state_action)
 
 
+
     def apply_trafo_predict(self, KERN_SIZE, current_state, enc6, hidden5, prev_image, prev_pix_distrib1,
                             prev_pix_distrib2, reuse, state_action):
         """
@@ -279,6 +289,7 @@ class Base_Prediction_Model(object):
         self.gen_states.append(current_state)
         return current_state
 
+
     def apply_cdna(self, enc6, hidden5, prev_image, prev_pix_distrib1, prev_pix_distrib2, reuse):
         if 'gen_pix' in self.conf:
             enc7 = slim.layers.conv2d_transpose(
@@ -291,19 +302,19 @@ class Base_Prediction_Model(object):
         if 'mov_bckgd' in self.conf:
             self.extra_masks = self.num_masks
         cdna_input = tf.reshape(hidden5, [int(self.batch_size), -1])
-        new_transformed, cdna_kerns = self.cdna_transformation(prev_image,
+        new_transformed, cdna_kerns = cdna_transformation(prev_image,
                                                                cdna_input,
                                                                reuse_sc=reuse)
         transformed_l += new_transformed
         self.moved_images.append(transformed_l)
         if self.pix_distrib1 != None:
-            tf_distrib_ndesig1, _ = self.cdna_transformation(prev_pix_distrib1,
+            tf_distrib_ndesig1, _ = cdna_transformation(prev_pix_distrib1,
                                                                  cdna_input,
                                                                  reuse_sc=True)
             self.moved_pix_distrib1.append(tf_distrib_ndesig1)
 
             if 'ndesig' in self.conf:
-                tf_distrib_ndesig2, _ = self.cdna_transformation(
+                tf_distrib_ndesig2, _ = cdna_transformation(
                     prev_pix_distrib2,
                     cdna_input,
                     reuse_sc=True)
@@ -321,12 +332,12 @@ class Base_Prediction_Model(object):
         # Using largest hidden state for predicting untied conv kernels.
         trafo_input = slim.layers.conv2d_transpose(
             enc6, KERN_SIZE ** 2, 1, stride=1, scope='convt4_cam2')
-        transformed_l = [self.dna_transformation(prev_image, trafo_input, self.conf['kern_size'])]
+        transformed_l = [dna_transformation(prev_image, trafo_input, self.conf['kern_size'])]
         if self.pix_distrib1 != None:
-            transf_distrib_ndesig1 = [self.dna_transformation(prev_pix_distrib1, trafo_input, KERN_SIZE)]
+            transf_distrib_ndesig1 = [dna_transformation(prev_pix_distrib1, trafo_input, KERN_SIZE)]
             if 'ndesig' in self.conf:
                 transf_distrib_ndesig2 = [
-                    self.dna_transformation(prev_pix_distrib2, trafo_input, KERN_SIZE)]
+                    dna_transformation(prev_pix_distrib2, trafo_input, KERN_SIZE)]
             else:
                 transf_distrib_ndesig2 = None
         else:
@@ -559,120 +570,6 @@ class Base_Prediction_Model(object):
 
             vecs.append(tf.stack([vec_r,vec_c], axis=1))
         return vecs
-
-    ## Utility functions
-    def stp_transformation(self, prev_image, stp_input, num_masks, reuse= None, suffix = None):
-        """Apply spatial transformer predictor (STP) to previous image.
-
-        Args:
-          prev_image: previous image to be transformed.
-          stp_input: hidden layer to be used for computing STN parameters.
-          num_masks: number of masks and hence the number of STP transformations.
-        Returns:
-          List of images transformed by the predicted STP parameters.
-        """
-        # Only import spatial transformer if needed.
-        from python_visual_mpc.video_prediction.transformer.spatial_transformer import transformer
-
-        identity_params = tf.convert_to_tensor(
-            np.array([1.0, 0.0, 0.0, 0.0, 1.0, 0.0], np.float32))
-        transformed = []
-        trafos = []
-        for i in range(num_masks):
-            params = slim.layers.fully_connected(
-                stp_input, 6, scope='stp_params' + str(i) + suffix,
-                activation_fn=None,
-                reuse= reuse) + identity_params
-            outsize = (prev_image.get_shape()[1], prev_image.get_shape()[2])
-            transformed.append(transformer(prev_image, params, outsize))
-            trafos.append(params)
-
-        return transformed, trafos
-
-
-    def dna_transformation(self, prev_image, dna_input, DNA_KERN_SIZE):
-        """Apply dynamic neural advection to previous image.
-
-        Args:
-          prev_image: previous image to be transformed.
-          dna_input: hidden lyaer to be used for computing DNA transformation.
-        Returns:
-          List of images transformed by the predicted CDNA kernels.
-        """
-        # Construct translated images.
-        pad_len = int(np.floor(DNA_KERN_SIZE / 2))
-        prev_image_pad = tf.pad(prev_image, [[0, 0], [pad_len, pad_len], [pad_len, pad_len], [0, 0]])
-        image_height = int(prev_image.get_shape()[1])
-        image_width = int(prev_image.get_shape()[2])
-
-        inputs = []
-        for xkern in range(DNA_KERN_SIZE):
-            for ykern in range(DNA_KERN_SIZE):
-                inputs.append(
-                    tf.expand_dims(
-                        tf.slice(prev_image_pad, [0, xkern, ykern, 0],
-                                 [-1, image_height, image_width, -1]), [3]))
-        inputs = tf.concat(axis=3, values=inputs)
-
-        # Normalize channels to 1.
-        kernel = tf.nn.relu(dna_input - RELU_SHIFT) + RELU_SHIFT
-        kernel = tf.expand_dims(
-            kernel / tf.reduce_sum(
-                kernel, [3], keep_dims=True), [4])
-
-        return tf.reduce_sum(kernel * inputs, [3], keep_dims=False)
-
-    def cdna_transformation(self, prev_image, cdna_input, reuse_sc=None, scope=None):
-        """Apply convolutional dynamic neural advection to previous image.
-
-        Args:
-          prev_image: previous image to be transformed.
-          cdna_input: hidden lyaer to be used for computing CDNA kernels.
-          num_masks: the number of masks and hence the number of CDNA transformations.
-          color_channels: the number of color channels in the images.
-        Returns:
-          List of images transformed by the predicted CDNA kernels.
-        """
-        batch_size = int(cdna_input.get_shape()[0])
-        height = int(prev_image.get_shape()[1])
-        width = int(prev_image.get_shape()[2])
-
-        DNA_KERN_SIZE = self.conf['kern_size']
-        num_masks = self.conf['num_masks']
-        color_channels = int(prev_image.get_shape()[3])
-
-        if scope == None:
-            scope = 'cdna_params'
-        # Predict kernels using linear function of last hidden layer.
-        cdna_kerns = slim.layers.fully_connected(
-            cdna_input,
-            DNA_KERN_SIZE * DNA_KERN_SIZE * num_masks,
-            scope=scope,
-            activation_fn=None,
-            reuse = reuse_sc)
-
-        # Reshape and normalize.
-        cdna_kerns = tf.reshape(
-            cdna_kerns, [batch_size, DNA_KERN_SIZE, DNA_KERN_SIZE, 1, num_masks])
-        cdna_kerns = tf.nn.relu(cdna_kerns - RELU_SHIFT) + RELU_SHIFT
-        norm_factor = tf.reduce_sum(cdna_kerns, [1, 2, 3], keep_dims=True)
-        cdna_kerns /= norm_factor
-        cdna_kerns_summary = cdna_kerns
-
-        # Transpose and reshape.
-        cdna_kerns = tf.transpose(cdna_kerns, [1, 2, 0, 4, 3])
-        cdna_kerns = tf.reshape(cdna_kerns, [DNA_KERN_SIZE, DNA_KERN_SIZE, batch_size, num_masks])
-        prev_image = tf.transpose(prev_image, [3, 1, 2, 0])
-
-        transformed = tf.nn.depthwise_conv2d(prev_image, cdna_kerns, [1, 1, 1, 1], 'SAME')
-
-        # Transpose and reshape.
-        transformed = tf.reshape(transformed, [color_channels, height, width, batch_size, num_masks])
-        transformed = tf.transpose(transformed, [3, 1, 2, 0, 4])
-        transformed = tf.unstack(value=transformed, axis=-1)
-
-        return transformed, cdna_kerns
-
 
 def scheduled_sample(ground_truth_x, generated_x, batch_size, num_ground_truth):
     """Sample batch with specified mix of ground truth and generated data_files points.
---
+++ python_visual_mpc/video_prediction/basecls/utils/transformations.py
@@ -0,0 +1,118 @@
+import numpy as np
+import tensorflow as tf
+
+RELU_SHIFT = 1e-12
+
+def dna_transformation(self, prev_image, dna_input, DNA_KERN_SIZE):
+    """Apply dynamic neural advection to previous image.
+
+    Args:
+      prev_image: previous image to be transformed.
+      dna_input: hidden lyaer to be used for computing DNA transformation.
+    Returns:
+      List of images transformed by the predicted CDNA kernels.
+    """
+    # Construct translated images.
+    pad_len = int(np.floor(DNA_KERN_SIZE / 2))
+    prev_image_pad = tf.pad(prev_image, [[0, 0], [pad_len, pad_len], [pad_len, pad_len], [0, 0]])
+    image_height = int(prev_image.get_shape()[1])
+    image_width = int(prev_image.get_shape()[2])
+
+    inputs = []
+    for xkern in range(DNA_KERN_SIZE):
+        for ykern in range(DNA_KERN_SIZE):
+            inputs.append(
+                tf.expand_dims(
+                    tf.slice(prev_image_pad, [0, xkern, ykern, 0],
+                             [-1, image_height, image_width, -1]), [3]))
+    inputs = tf.concat(axis=3, values=inputs)
+
+    # Normalize channels to 1.
+    kernel = tf.nn.relu(dna_input - RELU_SHIFT) + RELU_SHIFT
+    kernel = tf.expand_dims(
+        kernel / tf.reduce_sum(
+            kernel, [3], keep_dims=True), [4])
+
+    return tf.reduce_sum(kernel * inputs, [3], keep_dims=False)
+
+
+def cdna_transformation(prev_image, cdna_input, reuse_sc=None, scope=None):
+    """Apply convolutional dynamic neural advection to previous image.
+
+    Args:
+      prev_image: previous image to be transformed.
+      cdna_input: hidden lyaer to be used for computing CDNA kernels.
+      num_masks: the number of masks and hence the number of CDNA transformations.
+      color_channels: the number of color channels in the images.
+    Returns:
+      List of images transformed by the predicted CDNA kernels.
+    """
+    batch_size = int(cdna_input.get_shape()[0])
+    height = int(prev_image.get_shape()[1])
+    width = int(prev_image.get_shape()[2])
+
+    DNA_KERN_SIZE = self.conf['kern_size']
+    num_masks = self.conf['num_masks']
+    color_channels = int(prev_image.get_shape()[3])
+
+    if scope == None:
+        scope = 'cdna_params'
+    # Predict kernels using linear function of last hidden layer.
+    cdna_kerns = slim.layers.fully_connected(
+        cdna_input,
+        DNA_KERN_SIZE * DNA_KERN_SIZE * num_masks,
+        scope=scope,
+        activation_fn=None,
+        reuse=reuse_sc)
+
+    # Reshape and normalize.
+    cdna_kerns = tf.reshape(
+        cdna_kerns, [batch_size, DNA_KERN_SIZE, DNA_KERN_SIZE, 1, num_masks])
+    cdna_kerns = tf.nn.relu(cdna_kerns - RELU_SHIFT) + RELU_SHIFT
+    norm_factor = tf.reduce_sum(cdna_kerns, [1, 2, 3], keep_dims=True)
+    cdna_kerns /= norm_factor
+    cdna_kerns_summary = cdna_kerns
+
+    # Transpose and reshape.
+    cdna_kerns = tf.transpose(cdna_kerns, [1, 2, 0, 4, 3])
+    cdna_kerns = tf.reshape(cdna_kerns, [DNA_KERN_SIZE, DNA_KERN_SIZE, batch_size, num_masks])
+    prev_image = tf.transpose(prev_image, [3, 1, 2, 0])
+
+    transformed = tf.nn.depthwise_conv2d(prev_image, cdna_kerns, [1, 1, 1, 1], 'SAME')
+
+    # Transpose and reshape.
+    transformed = tf.reshape(transformed, [color_channels, height, width, batch_size, num_masks])
+    transformed = tf.transpose(transformed, [3, 1, 2, 0, 4])
+    transformed = tf.unstack(value=transformed, axis=-1)
+
+    return transformed, cdna_kerns
+
+
+## Utility functions
+def stp_transformation(self, prev_image, stp_input, num_masks, reuse= None, suffix = None):
+    """Apply spatial transformer predictor (STP) to previous image.
+
+    Args:
+      prev_image: previous image to be transformed.
+      stp_input: hidden layer to be used for computing STN parameters.
+      num_masks: number of masks and hence the number of STP transformations.
+    Returns:
+      List of images transformed by the predicted STP parameters.
+    """
+    # Only import spatial transformer if needed.
+    from python_visual_mpc.video_prediction.transformer.spatial_transformer import transformer
+
+    identity_params = tf.convert_to_tensor(
+        np.array([1.0, 0.0, 0.0, 0.0, 1.0, 0.0], np.float32))
+    transformed = []
+    trafos = []
+    for i in range(num_masks):
+        params = slim.layers.fully_connected(
+            stp_input, 6, scope='stp_params' + str(i) + suffix,
+            activation_fn=None,
+            reuse= reuse) + identity_params
+        outsize = (prev_image.get_shape()[1], prev_image.get_shape()[2])
+        transformed.append(transformer(prev_image, params, outsize))
+        trafos.append(params)
+
+    return transformed, trafos
