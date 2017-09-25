3ac7db64ef717c85a59ed54a2fa6c7581e665640
febert <frederik.ebert@mytum.de>
2017-09-24 21:37:11 -0700

    sc

--- tensorflow_data/sawyer/wristrot0-9/conf.py
+++ tensorflow_data/sawyer/wristrot0-9/conf.py
@@ -2,7 +2,7 @@ import os
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
 # tf record data location:
-DATA_DIR = '/'.join(str.split(current_dir, '/')[:-3]) + '/pushing_data/wrist_rot_10-17/train'
+DATA_DIR = '/'.join(str.split(current_dir, '/')[:-3]) + '/pushing_data/wrist_rot_0-9/train'
 
 # local output directory
 OUT_DIR = current_dir + '/modeldata'
