53aa30ef9c39d8237e1c5096556dc1df0c81d217
febert <frederik.ebert@mytum.de>
2017-10-17 14:03:36 -0700

    sc

--- python_visual_mpc/region_proposal_networks/rpn_tracker.py
+++ python_visual_mpc/region_proposal_networks/rpn_tracker.py
@@ -6,8 +6,16 @@ import argparse
 import numpy as np
 import sys
 from python_visual_mpc.region_proposal_networks.Featurizer import BBProposer, AlexNetFeaturizer
-
+import cv2
 import pdb
+import os
+
+import glob
+import moviepy.editor as mpy
+
+def make_gif(im_list):
+    clip = mpy.ImageSequenceClip(im_list, fps=4)
+    clip.write_gif('modeldata/tracking.gif')
 
 class RPN_Tracker(object):
     def __init__(self):
@@ -16,20 +24,17 @@ class RPN_Tracker(object):
         self.proposer = BBProposer()
         self.featurizer = AlexNetFeaturizer()
 
+        self.sel_feature_vec = None
+
     def get_regions(self, image):
         self.clone = np.array(image)[:, :, :3].astype(np.float64)
         self.clone[:,:] -= np.array([122.7717, 102.9801, 115.9465 ])
-        boxes = [tuple(b) for b in self.proposer.extract_proposal(self.clone)]
+        self.boxes = [tuple(b) for b in self.proposer.extract_proposal(self.clone)]
         self.clone[:,:] += np.array([122.7717, 102.9801, 115.9465 ])
-        crops = [self.proposer.get_crop(b, self.clone) for b in boxes]
-        feats = {b: self.featurizer.getFeatures(c) for b,c in zip(boxes,crops)}
+        crops = [self.proposer.get_crop(b, self.clone) for b in self.boxes]
+        self.feats = {b: self.featurizer.getFeatures(c) for b,c in zip(self.boxes,crops)}
 
-
-
-        return boxes, feats
-
-
-    def plot(self):
+    def plot(self, interactive = False):
         window = Tkinter.Tk(className="Image")
         image = Image.fromarray(np.uint8(self.clone))
         canvas = Tkinter.Canvas(window, width=image.size[0], height=image.size[1])
@@ -39,6 +44,14 @@ class RPN_Tracker(object):
                                                image.size[1]//2,
                                                image=self.image_tk)
 
+        if interactive:
+            canvas.bind("<Button-1>", self.click)
+            self.canvas = canvas
+            self.canvas.bind("<Right>", self.nextbox)
+            self.canvas.bind("<Left>", self.prevbox)
+            self.canvas.bind("<Return>", self.savefeats)
+            self.canvas.focus_set()
+            self.canvas.pack()
 
     def draw_cross(self, coord, im):
         """
@@ -55,7 +68,6 @@ class RPN_Tracker(object):
         rmax = r + h
         cmin = c - h
         cmax = c + h
-
 
         img_h = im.shape[0]
         img_w = im.shape[1]
@@ -77,8 +89,8 @@ class RPN_Tracker(object):
         box format is x1, y1, x2, y2 = colstart, rowstart, colend, rowend
         """
 
-
-        boxes, feats = self.get_regions(image)
+        self.get_regions(image)
+        boxes = self.boxes
 
         # for b in boxes:
         #     self.proposer.draw_box(b, self.clone, 0)  # red
@@ -111,9 +123,105 @@ class RPN_Tracker(object):
                 valid_region[1] < goal_point[1] < valid_region[3]:
                 break
 
-
         self.draw_cross(desig_point, self.clone)
         self.draw_cross(goal_point, self.clone)
 
         self.plot()
-        #TODO: filter out boxes
+
+    ### for tracking:
+    def test_tracking(self, files):
+
+        im_list = []
+        for t in range(95):
+            [imfile] = glob.glob(files + "/main_full_cropped_im{}_*.jpg".format(str(t).zfill(2)))
+            img = np.array(Image.open(imfile))
+
+            self.get_regions(img)
+            if t == 0:
+                for y in range(self.clone.shape[0]):
+                    for x in range(self.clone.shape[1]):
+                        self.pix2boxes[(x, y)] = []
+                for b in self.boxes:
+                    x1, y1, x2, y2 = (int(bi) for bi in b)
+                    for y in range(y1, y2):
+                        for x in range(x1, x2):
+                            self.pix2boxes[(x, y)].append(b)
+                    self.proposer.draw_box(b, self.clone, 0)
+
+                self.plot(interactive=True)
+                Tkinter.mainloop()
+            else:
+                self.get_regions(img)
+                dist_box_dict = {}
+                dists = []
+                for box in self.feats.keys():
+                    dist = np.linalg.norm(self.sel_feature_vec - self.feats[box])
+                    dist_box_dict[dist] = box
+                    dists.append(dist)
+
+                least_dist = min(dists)
+                best_box = dist_box_dict[least_dist]
+
+                self.proposer.draw_box(best_box, self.clone, 1)
+                cv2.imshow("Tracking", cv2.cvtColor(self.clone.astype(np.uint8), cv2.COLOR_BGR2RGB))
+                k = cv2.waitKey(1) & 0xff
+                if k == 27: break
+
+                small_rgb = cv2.resize(self.clone, (0, 0), fx=0.6, fy=0.6)
+                if t % 5 == 0:
+                    im_list.append(small_rgb)
+
+        make_gif(im_list)
+
+    def drawbox(self,box):
+        self.proposer.draw_box(box, self.clone, 1)
+        image = Image.fromarray(np.uint8(self.clone))
+        self.image_tk = ImageTk.PhotoImage(image)
+        self.canvas.itemconfig(self.canvasimage, image=self.image_tk)
+
+    def refreshboxes(self):
+        for b in self.boxes:
+            self.proposer.draw_box(b, self.clone, 0)
+        image = Image.fromarray(np.uint8(self.clone))
+        self.image_tk = ImageTk.PhotoImage(image)
+        self.canvas.itemconfig(self.canvasimage, image=self.image_tk)
+
+    def nextbox(self, event):
+        self.listpos+=1
+        if self.listpos == len(self.boxlist):
+            self.listpos = 0
+        box = self.boxlist[self.listpos]
+        self.refreshboxes()
+        self.drawbox(box)
+
+    def prevbox(self, event):
+        self.listpos-=1
+        if self.listpos == -1:
+            self.listpos = len(self.boxlist)-1
+        box = self.boxlist[self.listpos]
+        self.refreshboxes()
+        self.drawbox(box)
+
+    def savefeats(self, event):
+        print "You selected the box at", self.boxlist[self.listpos]
+        self.sel_feature_vec = self.feats[self.boxlist[self.listpos]]
+
+    def click(self,event):
+        x,y = event.x, event.y
+        self.refreshboxes()
+        self.boxlist = self.pix2boxes[(x,y)]
+        self.listpos = 0
+        if len(self.boxlist) > 0:
+            print "There are", len(self.boxlist), "boxes around this pixel."
+            self.drawbox(self.boxlist[self.listpos])
+
+        else:
+            print "No box was selected, please try again"
+
+
+
+if __name__ == '__main__':
+    r = RPN_Tracker()
+    import python_visual_mpc
+    image_files = os.path.dirname(python_visual_mpc.__file__) + '/flow/sparse_tracking/testdata/img2'
+    r.test_tracking(image_files)
---
+++ python_visual_mpc/video_prediction/modelvariants/bilinup_model.py
@@ -0,0 +1,131 @@
+import numpy as np
+import tensorflow as tf
+
+import tensorflow.contrib.slim as slim
+from tensorflow.contrib.layers.python import layers as tf_layers
+from python_visual_mpc.video_prediction.lstm_ops12 import basic_conv_lstm_cell
+from python_visual_mpc.misc.zip_equal import zip_equal
+
+import collections
+import cPickle
+from python_visual_mpc.video_prediction.utils_vpred.animate_tkinter import Visualizer_tkinter
+import pdb
+
+# Amount to use when lower bounding tensors
+RELU_SHIFT = 1e-12
+from python_visual_mpc.video_prediction.basecls.prediction_model_basecls import Base_Prediction_Model
+
+
+class Bilinup_Model(Base_Prediction_Model):
+    def __init__(self,
+                conf = None,
+                trafo_pix = True,
+                load_data = True,
+                mode=True):
+        Base_Prediction_Model.__init__(self,
+                                        conf = conf,
+                                        trafo_pix = trafo_pix,
+                                        load_data = load_data,
+                                        mode=mode)
+
+
+    def build_network_core(self, action, current_state, input_image):
+        lstm_func = basic_conv_lstm_cell
+
+        print 'using bilinear upsampling'
+
+        with slim.arg_scope(
+                [lstm_func, slim.layers.conv2d, slim.layers.fully_connected,
+                 tf_layers.layer_norm, slim.layers.conv2d_transpose],
+                reuse=self.reuse):
+
+            enc0 = slim.layers.conv2d(  # 32x32x32
+                input_image,
+                32, [5, 5],
+                stride=2,
+                scope='scale1_conv1',
+                normalizer_fn=tf_layers.layer_norm,
+                normalizer_params={'scope': 'layer_norm1'})
+            hidden1, self.lstm_state1 = self.lstm_func(  # 32x32x16
+                enc0, self.lstm_state1, self.lstm_size[0], scope='state1')
+            hidden1 = tf_layers.layer_norm(hidden1, scope='layer_norm2')
+
+            enc1 = slim.layers.conv2d(  # 16x16x16
+                hidden1, hidden1.get_shape()[3], [3, 3], stride=2, scope='conv2')
+            hidden3, self.lstm_state3 = self.lstm_func(  # 16x16x32
+                enc1, self.lstm_state3, self.lstm_size[1], scope='state3')
+            hidden3 = tf_layers.layer_norm(hidden3, scope='layer_norm4')
+
+            enc2 = slim.layers.conv2d(  # 8x8x32
+                hidden3, hidden3.get_shape()[3], [3, 3], stride=2, scope='conv3')
+
+            if not 'ignore_state_action' in self.conf:
+                # Pass in state and action.
+                state_action = tf.concat(axis=1, values=[action, current_state])
+
+                smear = tf.reshape(state_action,[int(self.batch_size), 1, 1, int(state_action.get_shape()[1])])
+                smear = tf.tile(
+                    smear, [1, int(enc2.get_shape()[1]), int(enc2.get_shape()[2]), 1])
+
+                enc2 = tf.concat(axis=3, values=[enc2, smear])
+            else:
+                print 'ignoring states and actions'
+            enc3 = slim.layers.conv2d(  # 8x8x32
+                enc2, hidden3.get_shape()[3], [1, 1], stride=1, scope='conv4')
+            hidden5, self.lstm_state5 = self.lstm_func(  # 8x8x64
+                enc3, self.lstm_state5, self.lstm_size[2], scope='state5')
+            hidden5 = tf_layers.layer_norm(hidden5, scope='layer_norm6')
+
+            hidden5_up = tf.image.resize_images(  # 16x16x64
+                hidden5,
+                [16, 16],
+                method=tf.image.ResizeMethod.BILINEAR,
+                align_corners=False)
+
+            enc4 = slim.layers.conv2d(  # 16x16x64
+                hidden5_up, 64, 3, stride=1, scope='conv5')
+            hidden6, self.lstm_state6 = self.lstm_func(  # 16x16x32
+                enc4, self.lstm_state6, self.lstm_size[3], scope='state6')
+            hidden6 = tf_layers.layer_norm(hidden6, scope='layer_norm7')
+            if 'noskip' not in self.conf:
+                # Skip connection.
+                hidden6 = tf.concat(axis=3, values=[hidden6, enc1])  # both 16x16
+
+            hidden6_up = tf.image.resize_images(  # 32x32x64
+                hidden6,
+                [32, 32],
+                method=tf.image.ResizeMethod.BILINEAR,
+                align_corners=False)
+
+            enc5 = slim.layers.conv2d(  # 32x32x32
+                hidden6_up, 32, 3, stride=1, scope='conv6')
+            hidden7, self.lstm_state7 = self.lstm_func(  # 32x32x16
+                enc5, self.lstm_state7, self.lstm_size[4], scope='state7')
+            hidden7 = tf_layers.layer_norm(hidden7, scope='layer_norm8')
+            if not 'noskip' in self.conf:
+                # Skip connection.
+                hidden7 = tf.concat(axis=3, values=[hidden7, enc0])  # both 32x32
+
+            hidden7_up = tf.image.resize_images(  # 64x64x16
+                hidden7,
+                [64, 64],
+                method=tf.image.ResizeMethod.BILINEAR,
+                align_corners=False)
+
+            enc6 = slim.layers.conv2d(  # 64x64x16
+                hidden7_up,
+                16, 3, stride=1, scope='conv7',
+                normalizer_fn=tf_layers.layer_norm,
+                normalizer_params={'scope': 'layer_norm9'})
+
+            if current_state != None:
+                current_state = slim.layers.fully_connected(
+                    state_action,
+                    int(current_state.get_shape()[1]),
+                    scope='state_pred',
+                    activation_fn=None)
+            self.gen_states.append(current_state)
+
+            self.apply_trafo_predict(enc6, hidden5)
+
+            return current_state
