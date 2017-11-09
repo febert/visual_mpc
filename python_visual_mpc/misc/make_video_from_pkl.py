6dedde158bc6b45fccc0d49da2a44148edf4ef69
febert <frederik.ebert@mytum.de>
2017-11-08 21:29:04 -0800

    sc

--- python_visual_mpc/misc/make_video_from_pkl.py
+++ python_visual_mpc/misc/make_video_from_pkl.py
@@ -15,19 +15,22 @@ def save_video_mp4(filename, frames):
 def main():
 
     # file = '/home/frederik/Documents/lsdc/experiments/cem_exp/benchmarks_sawyer/predprop_1stimg_bckgd/exp/unseen_clutter/verbose/gen_image_t3.pkl'
-    pklfile = '/home/febert/Documents/catkin_ws/src/visual_mpc/experiments/cem_exp/benchmarks_sawyer/wristrot/videos/record_cem1/pred.pkl'
 
-    dict = cPickle.load(open(pklfile, "rb"))
+    for cem_dir in range(5):
+        filedir = '/home/frederik/Documents/catkin_ws/src/visual_mpc/experiments/cem_exp/benchmarks_sawyer/wristrot/videos/record_cem{}'.format(cem_dir)
+        print 'processing dir ', filedir
+        dict = cPickle.load(open(filedir+'/pred.pkl', "rb"))
 
-    dest_dir = '/home/febert/Documents/catkin_ws/src/visual_mpc/experiments/cem_exp/benchmarks_sawyer/wristrot/videos/record_cem1/'
+        dest_dir = filedir
 
-    for t in range(1,6,2):
+        t = 3
 
-        # for itr in range(3):
-        itr = 2
-        gen_images = dict['gen_images_t{}_iter{}'.format(t,itr)]
-        gen_images = [im[0] for im in gen_images]
-        save_video_mp4(dest_dir + 'gen_images_t{}_iter{}'.format(t,itr), gen_images)
+        for b in range(10):
+            # for itr in range(3):
+            itr = 2
+            gen_images = dict['gen_images_t{}_iter{}'.format(t,itr)]
+            gen_images = [im[b] for im in gen_images]
+            save_video_mp4(dest_dir + '/gen_images_b{}_t{}_iter{}'.format(b, t,itr), gen_images)
 
 if __name__ == '__main__':
     main()
