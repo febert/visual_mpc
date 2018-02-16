4d91acf522fd986ba73c4a5bb51fc00310c16757
Sudeep Dasari <sdasari@berkeley.edu>
2018-02-16 15:38:04 -0800

    creating grasping experiment config, and reenabled grasp motors

---
+++ pushing_data/cartgripper_det_grasp/hyperparams.py
@@ -0,0 +1,63 @@
+""" Hyperparameters for Large Scale Data Collection (LSDC) """
+from __future__ import division
+import os.path
+
+import numpy as np
+
+from python_visual_mpc.visual_mpc_core.algorithm.random_policy import Randompolicy
+from python_visual_mpc.visual_mpc_core.agent.agent_mjc import AgentMuJoCo
+
+IMAGE_WIDTH = 64
+IMAGE_HEIGHT = 64
+IMAGE_CHANNELS = 3
+
+BASE_DIR = '/'.join(str.split(__file__, '/')[:-1])
+
+import python_visual_mpc
+DATA_DIR = '/'.join(str.split(python_visual_mpc.__file__, '/')[:-2])
+
+agent = {
+    'type': AgentMuJoCo,
+    'data_save_dir': BASE_DIR + '/train',
+    'filename': DATA_DIR+'/mjc_models/cartgripper_grasp.xml',
+    'filename_nomarkers': DATA_DIR+'/mjc_models/cartgripper_grasp.xml',
+    'data_collection': True,
+    'sample_objectpos':'',
+    'adim':4,
+    'sdim':10,
+    'xpos0': np.zeros(5), #initialize state dimension to 5 zeros
+    'dt': 0.05,
+    'substeps': 20,  #6
+    'T': 15,
+    'skip_first': 40,   #skip first N time steps to let the scene settle
+    'additional_viewer': True,
+    'image_height' : 48,
+    'image_width' : 64,
+    'viewer_image_height' : 480,
+    'viewer_image_width' : 640,
+    'image_channels' : 3,
+    'num_objects': 4,
+    'novideo':'',
+    'gen_xml':10,   #generate xml every nth trajecotry
+    'randomize_ballinitpos':'', #randomize x, y
+    # 'displacement_threshold':0.1,
+}
+
+policy = {
+    'type' : Randompolicy,
+    'nactions': 5,
+    'repeats': 3, # number of repeats for each action
+    'initial_std': 10.,   #std dev. in xy
+    'initial_std_lift': 1e-5,   #std dev. in xy
+    # 'initial_std_grasp': 1e-5,   #std dev. in xy
+}
+
+config = {
+    'traj_per_file':128,
+    'save_data': True,
+    'start_index':0,
+    'end_index': 60000,
+    'agent': agent,
+    'policy': policy,
+    'ngroup': 1000
+}
