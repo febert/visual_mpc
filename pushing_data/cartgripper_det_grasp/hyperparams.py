added rotation hinge, fixed too many contact error, and fixed box squishing

pushing_data/cartgripper_det_grasp/hyperparams.py
@@ -23,9 +23,9 @@ agent = {
     'filename_nomarkers': DATA_DIR+'/mjc_models/cartgripper_grasp.xml',
     'data_collection': True,
     'sample_objectpos':'',
-    'adim':4,
-    'sdim':10,
-    'xpos0': np.array([0., 0., 0.05, 0., 0.]), #initialize state dimension to 5 zeros
+    'adim':5,
+    'sdim':12,
+    'xpos0': np.array([0., 0., 0.05, 0., 0., 0.]), #initialize state dimension to 5 zeros
     'dt': 0.05,
     'substeps': 50,  #6
     'T': 100,

python_visual_mpc/visual_mpc_core/agent/agent_mjc.py
@@ -189,8 +189,16 @@ class AgentMuJoCo(object):
 
         # Take the sample.
         for t in range(self.T):
+            # print 'qpos dim', self._model.data.qpos.shape
+            # print 'qvel dim', self._model.data.qvel.shape
+
             qpos_dim = self.sdim / 2  # the states contains pos and vel
             traj.X_full[t, :] = self._model.data.qpos[:qpos_dim].squeeze()
+            # print 'qpos', self._model.data.qpos.squeeze()
+            # print 'ctrl', self._model.data.ctrl
+            # print 1 / 0
+
+
             traj.Xdot_full[t, :] = self._model.data.qvel[:qpos_dim].squeeze()
             traj.X_Xdot_full[t, :] = np.concatenate([traj.X_full[t, :], traj.Xdot_full[t, :]])
             assert self._model.data.qpos.shape[0] == qpos_dim + 7 * self._hyperparams['num_objects']
@@ -235,7 +243,9 @@ class AgentMuJoCo(object):
                     ctrl[2] -= self._model.data.qpos[2].squeeze()
 
                 self._model.data.ctrl = ctrl
+                # print 'ncon', self._model.data.ncon
                 self._model.step()
+
 
             if 'touch' in self._hyperparams:
                 traj.touchdata[t, :] = accum_touch.squeeze()
@@ -272,7 +282,7 @@ class AgentMuJoCo(object):
         if any(zval < -2e-2 for zval in end_zpos):
             print 'object fell out!!!'
             traj_ok = False
-
+        print 'ret'
         return traj_ok, traj
 
     def save_goal_image_conf(self, traj):

python_visual_mpc/visual_mpc_core/algorithm/det_grasp_policy.py
@@ -44,26 +44,27 @@ class DeterministicGraspPolicy(Policy):
             actions = np.zeros(self.adim)
             actions[:2] = traj.Object_pose[t, 0, :2]
             actions[2] = self.agentparams['ztarget']
-            actions[3] = -100
+            actions[3] = 3.14/4
+            actions[-1] = -100
             return actions, None
 
         elif self.drop:
             actions = np.zeros(self.adim)
             actions[:2] = traj.Object_pose[t, 0, :2]
             actions[2] = -0.08
-            actions[3] = -100
+            actions[-1] = -100
             return actions, None
         elif self.lift:
             actions = np.zeros(self.adim)
             actions[:2] = traj.Object_pose[t, 0, :2]
             actions[2] = self.agentparams['ztarget']
-            actions[3] = 21
+            actions[-1] = 21
 
             return actions, None
         elif self.grasp:
             actions = np.zeros(self.adim)
             actions[:2] = traj.Object_pose[t, 0, :2]
             actions[2] = -0.08
-            actions[3] = 5. / 10 * self.graspTime
+            actions[-1] = 5. / 10 * self.graspTime
             self.graspTime += 1
             return actions, None