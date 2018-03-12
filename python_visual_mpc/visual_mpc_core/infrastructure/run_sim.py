1b9c0852afb3f308dab3f047077ce02171a46a97
febert <frederik.ebert@mytum.de>
2018-03-12 16:24:59 -0700

    sc

--- python_visual_mpc/visual_mpc_core/infrastructure/run_sim.py
+++ python_visual_mpc/visual_mpc_core/infrastructure/run_sim.py
@@ -30,7 +30,7 @@ class Sim(object):
         self.agentparams = config['agent']
 
         self._data_save_dir = os.environ['VMPC_DATA_DIR'] + self.agentparams['data_save_dir']
-        self._timing_file = self._data_save_dir + '/timing_file{}'.format(os.getpid())
+        self._timing_file = self._hyperparams['current_dir'] + '/timing_file{}.txt'.format(os.getpid())
         with open(self._timing_file, 'wb') as f:
             f.write("timing \n")
 
