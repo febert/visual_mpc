ae54cc565cc9006e39787a21fc8cdb46dc948c9f
febert <frederik.ebert@mytum.de>
2018-03-20 11:04:50 -0700

    vae encpool

--- python_visual_mpc/misc/minimal_mujoco_1.5.py
+++ python_visual_mpc/misc/minimal_mujoco_1.5.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"run this with LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so:/usr/lib/nvidia-384/libGL.so"
 """
 Example of how bodies interact with each other. For a body to be able to
 move it needs to have joints. In this example, the "robot" is a red ball
@@ -7,6 +8,7 @@ On the floor, there's a cylinder with X and Y slide joints, so it can
 be pushed around with the robot. There's also a box without joints. Since
 the box doesn't have joints, it's fixed and can't be pushed around.
 """
+
 from mujoco_py import load_model_from_xml,load_model_from_path, MjSim, MjViewer
 import math
 import os
@@ -20,15 +22,16 @@ sim = MjSim(model)
 viewer = MjViewer(sim)
 t = 0
 while True:
-    sim.data.ctrl[0] = math.cos(t / 10.) * 0.01
-    sim.data.ctrl[1] = math.sin(t / 10.) * 0.01
+    sim.data.ctrl[0] = 0
+    sim.data.ctrl[1] = 0
     t += 1
     sim.step()
     viewer.render()
 
-    # image = sim.render(400, 400, camera_name="maincam")
-    # plt.imshow(image)
-    # plt.show()
+    image = sim.render(400, 400, camera_name="maincam")
+    print(sim.get_camera_params())
+    plt.imshow(image)
+    plt.show()
 
     if t > 100 and os.getenv('TESTING') is not None:
         break
