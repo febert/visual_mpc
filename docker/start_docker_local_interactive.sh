17161253b00943792912e66f0a6fbde4e32d36bb
febert <frederik.ebert@mytum.de>
2018-03-22 12:04:25 -0700

    sc

--- docker/start_docker_local_interactive.sh
+++ docker/start_docker_local_interactive.sh
@@ -1,5 +1,5 @@
 nvidia-docker run  -v /mnt/sda1/pushing_data:/workspace/pushing_data \
-                        -v /home/frederik/Documents/catkin_ws/src/visual_mpc/tensorflow_data/gdn/startgoal_shad/modeldata:/results \
+                        -v /home/frederik/Desktop:/outputs \
 -it -p 8888:8888 \
 nvcr.io/ucb_rail8888/tf1.4_gpu:based_nvidia \
 /bin/bash -c \
