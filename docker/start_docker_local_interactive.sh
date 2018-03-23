53e843631aa9c35f7375ff769515c7be164d3a3e
febert <frederik.ebert@mytum.de>
2018-03-22 21:07:01 -0700

    mjc dockerfile

--- docker/start_docker_local_interactive.sh
+++ docker/start_docker_local_interactive.sh
@@ -4,8 +4,7 @@ nvidia-docker run  -v /mnt/sda1/pushing_data:/workspace/pushing_data \
 nvcr.io/ucb_rail8888/tf1.4_gpu:based_nvidia \
 /bin/bash -c \
 "export VMPC_DATA_DIR=/workspace/pushing_data;
-export MUJOCO_PY_MJKEY_PATH=/workspace/visual_mpc/mujoco/mjpro131/mjkey.txt;
-export MUJOCO_PY_MJPRO_PATH=/workspace/visual_mpc/mujoco/mjpro131;
+export LANG C.UTF-8
 export PATH=/opt/conda/bin:/usr/local/mpi/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
 /bin/bash"
 
---
+++ docker/start_docker_mjc.sh
@@ -0,0 +1,8 @@
+nvidia-docker run  -v /mnt/sda1/pushing_data:/workspace/pushing_data \
+                   -v /home/frederik/Documents/catkin_ws/src/visual_mpc:/mount \
+-it \
+tf_mj1.5 \
+/bin/bash -c \
+"export VMPC_DATA_DIR=/workspace/pushing_data;
+/bin/bash"
+
