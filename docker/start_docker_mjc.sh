4a4c40929a841b5d70d9c8a9700c956f992596d1
febert <frederik.ebert@mytum.de>
2018-03-23 10:08:12 -0700

    sc

--- docker/start_docker_mjc.sh
+++ docker/start_docker_mjc.sh
@@ -1,5 +1,6 @@
 nvidia-docker run  -v /mnt/sda1/pushing_data:/workspace/pushing_data \
                    -v /home/frederik/Documents/catkin_ws/src/visual_mpc:/mount \
+                   -v /home/frederik/Desktop:/Desktop \
 -it \
 tf_mj1.5 \
 /bin/bash -c \
