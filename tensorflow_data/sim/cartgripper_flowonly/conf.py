f3ca27ca3dbf85db67631cc16f70c17f352cd4aa
febert <frederik.ebert@mytum.de>
2018-04-05 11:13:53 -0700

    sc

--- tensorflow_data/sim/cartgripper_flowonly/conf.py
+++ tensorflow_data/sim/cartgripper_flowonly/conf.py
@@ -31,7 +31,6 @@ configuration = {
 'file_visual': '',          # datafile used for making visualizations
 'kern_size': 17,            # size of DNA kerns
 'single_view':"",
-'use_len':14,                # number of steps used for training where the starting location is selected randomly within sequencelength
 '1stimg_bckgd':'',
 # 'visual_flowvec':'',
 'adim':3,
