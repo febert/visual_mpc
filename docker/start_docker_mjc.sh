c658deaa3d1cda8732b5b3fafc9f796cf0ee8a32
febert <frederik.ebert@mytum.de>
2018-04-08 15:00:15 -0700

    sc

--- docker/start_docker_mjc.sh
+++ docker/start_docker_mjc.sh
@@ -1,6 +1,7 @@
-nvidia-docker run  -v /mnt/sda1/pushing_data:/workspace/pushing_data \
-                   -v /home/$USER/Documents/catkin_ws/src/visual_mpc:/mount \
-                   -v /home/$USER/Desktop:/Desktop \
+# argument1: username, argument2: $VMPC_DATA_DIR
+nvidia-docker run  -v $2:/workspace/pushing_data \
+                   -v /home/$1/Documents/catkin_ws/src/visual_mpc:/mount \
+                   -v /home/$1/Desktop:/Desktop \
 -it \
 nvcr.io/ucb_rail8888/tf_mj1.5 \
 /bin/bash -c \
