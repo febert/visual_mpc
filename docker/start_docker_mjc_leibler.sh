8c80a9b8baa507706fce4893076401946d94035f
Sudeep Dasari <sdasari@berkeley.edu>
2018-04-11 20:32:33 -0700

    personal mjc start

---
+++ docker/start_docker_mjc_leibler.sh
@@ -0,0 +1,11 @@
+# argument1: username, argument2: $VMPC_DATA_DIR
+nvidia-docker run  -v $2:/workspace/pushing_data \
+                   -v /home/sudeep/Documents/visual_mpc:/mount \
+                   -v /home/sudeep/Desktop:/Desktop \
+-it \
+febert/tf_mj1.5:runmount \
+
+/bin/bash -c \
+"export VMPC_DATA_DIR=/workspace/pushing_data;
+/bin/bash"
+
