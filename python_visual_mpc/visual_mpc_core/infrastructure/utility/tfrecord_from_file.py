ae64f71975d408fabfd1ec9caa368ade3050a0e2
Sudeep Dasari <sdasari@berkeley.edu>
2018-04-12 12:58:31 -0700

    converted noisy data to records

--- python_visual_mpc/visual_mpc_core/infrastructure/utility/tfrecord_from_file.py
+++ python_visual_mpc/visual_mpc_core/infrastructure/utility/tfrecord_from_file.py
@@ -1,9 +1,10 @@
 import argparse
 import os
 import python_visual_mpc
-import imp
+import importlib.machinery
+import importlib.util
 import glob
-import cPickle as pkl
+import pickle as pkl
 import numpy as np
 import cv2
 from python_visual_mpc.visual_mpc_core.infrastructure.utility.save_tf_record import save_tf_record
@@ -11,7 +12,7 @@ class LoadTraj:
     def __init__(self):
         self.actions, self.X_Xdot_full, self._sample_images = None, None, None
 def main():
-    parser = argparse.ArgumentParser(description='run parllel data collection')
+    parser = argparse.ArgumentParser(description='run convert from directory to tf record')
     parser.add_argument('experiment', type=str, help='experiment name')
     parser.add_argument('output', type=str, help='new output dir')
 
@@ -22,8 +23,13 @@ def main():
     basepath = os.path.abspath(python_visual_mpc.__file__)
     basepath = '/'.join(str.split(basepath, '/')[:-2])
     data_coll_dir = basepath + '/pushing_data/' + exp_name
+
     hyperparams_file = data_coll_dir + '/hyperparams.py'
-    hyperparams = imp.load_source('hyperparams', hyperparams_file).config
+    loader = importlib.machinery.SourceFileLoader('mod_hyper', hyperparams_file)
+    spec = importlib.util.spec_from_loader(loader.name, loader)
+    conf = importlib.util.module_from_spec(spec)
+    loader.exec_module(conf)
+    hyperparams = conf.config
 
     traj_per_file = hyperparams['traj_per_file']
     agent_config = hyperparams['agent']
@@ -35,8 +41,8 @@ def main():
     img_height = agent_config['image_height']
     img_width = agent_config['image_width']
 
-    print 'loading from', data_dir
-    print 'saving to', out_dir
+    print('loading from', data_dir)
+    print('saving to', out_dir)
 
     traj_list = []
     counter = 0
@@ -46,9 +52,9 @@ def main():
     for g in traj_group_dirs:
         trajs = glob.glob(g + '/*')
         for t in trajs:
-            state_action = pkl.load(open(t + '/state_action.pkl', 'r'))
+            state_action = pkl.load(open(t + '/state_action.pkl', 'rb'))
             if np.sum(np.isnan(state_action['target_qpos'])) > 0:
-                print "FOUND NAN AT", t
+                print("FOUND NAN AT", t)
             else:
                 loaded_traj = LoadTraj()
                 loaded_traj.actions = state_action['actions']
