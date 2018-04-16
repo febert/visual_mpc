fe7d76541f1cc736358db423803dcec2fb6a20b7
febert <frederik.ebert@mytum.de>
2018-04-15 21:24:30 -0700

    adapting for alexmodel

--- python_visual_mpc/video_prediction/dynamic_rnn_model/alex_model_interface.py
+++ python_visual_mpc/video_prediction/dynamic_rnn_model/alex_model_interface.py
@@ -13,19 +13,11 @@ class Alex_Interface_Model(object):
                  build_loss = False
                  ):
 
-        self.gen_images = tf.unstack(self.model.gen_images, axis=1)
-        # self.gen_masks = tf.unstack(self.model.gen_masks)
-        self.prediction_flow = tf.unstack(self.model.gen_flow_map, axis=1)
-        if pix_distrib is not None:
-            self.gen_distrib = tf.unstack(self.model.gen_pix_distribs, axis=1)
-        self.gen_states = tf.unstack(self.model.gen_states)
-        self.gen_masks = self.model.gen_masks
-
-        with open(os.path.join(conf['checkpoint_dir'], "model_hparams.json")) as f:
+        with open(os.path.join(conf['json_dir'], "model_hparams.json")) as f:
             model_hparams_dict = json.loads(f.read())
             model_hparams_dict.pop('num_gpus', None)  # backwards-compatibility
 
-        self.model = SAVPVideoPredictionModel(mode='test', hparams_dict=model_hparams_dict)
+        self.m = SAVPVideoPredictionModel(mode='test', hparams_dict=model_hparams_dict)
 
         inputs = {
             'images':images,
@@ -34,10 +26,10 @@ class Alex_Interface_Model(object):
         }
         if pix_distrib is not None:
             inputs['pix_distribs'] = pix_distrib
-        self.model.build_graph(inputs)
+        self.m.build_graph(inputs)
 
-        self.gen_images = tf.unstack(self.model.outputs['gen_images'], axis=1)
-        self.gen_states = tf.unstack(self.model.outputs['gen_states'], axis=1)
+        self.gen_images = tf.unstack(self.m.outputs['gen_images'], axis=1)
+        self.gen_states = tf.unstack(self.m.outputs['gen_states'], axis=1)
 
         if pix_distrib is not None:
-            self.gen_pixdistrib = tf.unstack(self.model.outputs['gen_pix_distrib'], axis=1)
+            self.gen_distrib = tf.unstack(self.m.outputs['gen_pix_distribs'], axis=1)
