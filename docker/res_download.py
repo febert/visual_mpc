ed88b5df09029dc2313a492953afdf3fa8516d40
febert <frederik.ebert@mytum.de>
2018-04-17 09:10:48 -0700

    sc

--- docker/res_download.py
+++ docker/res_download.py
@@ -1,15 +1,14 @@
-import os
 import argparse
 import os
-
-
 parser = argparse.ArgumentParser(description='write json configuration for ngc')
 parser.add_argument('dir', type=str, help='relative path to script to withing visual_mpc directory')
 
 args = parser.parse_args()
 job_ids = []
 dir = args.dir
-for j in job_ids:
+job_start_id = 41212
+n = 10
+for j in range(n):
     cmd = "cd {}; ngc result download {} &".format(dir, j)
     print(cmd)
     os.system(cmd)
