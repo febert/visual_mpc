f2253dc44668ad3860e82d3c0c75fa391fe372bd
febert <frederik.ebert@mytum.de>
2018-04-16 21:26:20 -0700

    truncate actions

--- python_visual_mpc/visual_mpc_core/algorithm/random_policy.py
+++ python_visual_mpc/visual_mpc_core/algorithm/random_policy.py
@@ -1,10 +1,12 @@
 """ This file defines the linear Gaussian policy class. """
 import numpy as np
+import scipy
 
 from python_visual_mpc.visual_mpc_core.algorithm.policy import Policy
 
 from scipy.stats import multivariate_normal
 from python_visual_mpc.visual_mpc_core.algorithm.cem_controller_goalimage_sawyer import construct_initial_sigma
+from python_visual_mpc.visual_mpc_core.algorithm.cem_controller_goalimage_sawyer import truncate_movement
 
 class Randompolicy(Policy):
     """
@@ -37,7 +39,9 @@ class Randompolicy(Policy):
 
             if 'discrete_adim' in self.agentparams:
                 self.actions = discretize(self.actions, self.agentparams['discrete_adim'])
-                # print(self.actions)
+
+            if 'no_action_bound' not in self.policyparams:
+                self.actions = truncate_movement(self.actions, self.policyparams)
 
         return self.actions[t]
 
