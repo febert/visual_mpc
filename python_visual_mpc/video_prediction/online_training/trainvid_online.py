da75cf6f1f17f079d89c60e49691ef9781535875
febert <frederik.ebert@mytum.de>
2018-04-30 10:46:25 -0700

    sc

--- python_visual_mpc/video_prediction/online_training/trainvid_online.py
+++ python_visual_mpc/video_prediction/online_training/trainvid_online.py
@@ -47,76 +47,80 @@ def trainvid_online(replay_buffer, conf, gpu_id):
     vars = filter_vars(vars)
     saving_saver = tf.train.Saver(vars, max_to_keep=0)
 
+
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
-    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-    summary_writer = tf.summary.FileWriter(conf['event_log_dir'], graph=sess.graph, flush_secs=10)
+    g_vidpred= tf.Graph()
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, allow_soft_placement=True), graph=g_vidpred)
+    with sess.as_default():
+        with g_vidpred.as_default():
+            summary_writer = tf.summary.FileWriter(conf['event_log_dir'], graph=sess.graph, flush_secs=10)
 
-    vars = variable_checkpoint_matcher(conf, vars, conf['pretrained'])
-    loading_saver = tf.train.Saver(vars, max_to_keep=0)
-    load_checkpoint(conf, sess, loading_saver, conf['pretrained'])
+            vars = variable_checkpoint_matcher(conf, vars, conf['pretrained'])
+            loading_saver = tf.train.Saver(vars, max_to_keep=0)
+            load_checkpoint(conf, sess, loading_saver, conf['pretrained'])
 
-    print('-------------------------------------------------------------------')
-    print('verify current settings!! ')
-    for key in list(conf.keys()):
-        print(key, ': ', conf[key])
-    print('-------------------------------------------------------------------')
+            print('-------------------------------------------------------------------')
+            print('verify current settings!! ')
+            for key in list(conf.keys()):
+                print(key, ': ', conf[key])
+            print('-------------------------------------------------------------------')
 
-    tf.logging.set_verbosity(tf.logging.INFO)
+            tf.logging.set_verbosity(tf.logging.INFO)
 
-    starttime = datetime.now()
-    t_iter = []
-    for itr in range(0, conf['num_iterations'], 1):
-        tstart_rb_update = time.time()
-        replay_buffer.update()
-        # print("took {} to update the replay buffer".format(time.time() - tstart_rb_update))
+            starttime = datetime.now()
+            t_iter = []
+            for itr in range(0, conf['num_iterations'], 1):
+                tstart_rb_update = time.time()
+                replay_buffer.update()
+                # print("took {} to update the replay buffer".format(time.time() - tstart_rb_update))
 
-        t_startiter = datetime.now()
-        images, states, actions = replay_buffer.get_batch()
-        feed_dict = {model.iter_num: np.float32(itr),
-                     model.train_cond: 1,
-                     model.images_pl:images,
-                     model.actions_pl:actions,
-                     model.states_pl:states
-                     }
-        cost, _, summary_str = sess.run([model.loss, model.train_op, model.train_summ_op],
-                                        feed_dict)
+                t_startiter = datetime.now()
+                images, states, actions = replay_buffer.get_batch()
+                feed_dict = {model.iter_num: np.float32(itr),
+                             model.train_cond: 1,
+                             model.images_pl:images,
+                             model.actions_pl:actions,
+                             model.states_pl:states
+                             }
+                cost, _, summary_str = sess.run([model.loss, model.train_op, model.train_summ_op],
+                                                feed_dict)
 
-        t_iter.append((datetime.now() - t_startiter).seconds * 1e6 +  (datetime.now() - t_startiter).microseconds )
+                t_iter.append((datetime.now() - t_startiter).seconds * 1e6 +  (datetime.now() - t_startiter).microseconds )
 
-        if (itr) % 10 ==0:
-            tf.logging.info(str(itr) + ' ' + str(cost))
+                if (itr) % 10 ==0:
+                    tf.logging.info(str(itr) + ' ' + str(cost))
 
-        if (itr) % VIDEO_INTERVAL == 2 and hasattr(model, 'val_video_summaries'):
-            feed_dict = {model.iter_num: np.float32(itr),
-                         model.train_cond: 0,
-                         model.images_pl:images,
-                         model.actions_pl:actions,
-                         model.states_pl:states
-                         }
-            video_proto = sess.run(model.val_video_summaries, feed_dict = feed_dict)
-            summary_writer.add_summary(convert_tensor_to_gif_summary(video_proto), itr)
+                if (itr) % VIDEO_INTERVAL == 2 and hasattr(model, 'val_video_summaries'):
+                    feed_dict = {model.iter_num: np.float32(itr),
+                                 model.train_cond: 0,
+                                 model.images_pl:images,
+                                 model.actions_pl:actions,
+                                 model.states_pl:states
+                                 }
+                    video_proto = sess.run(model.val_video_summaries, feed_dict = feed_dict)
+                    summary_writer.add_summary(convert_tensor_to_gif_summary(video_proto), itr)
 
-        if (itr) % conf['save_interval'] == 2:
-            tf.logging.info('Saving model to' + conf['output_dir'])
-            saving_saver.save(sess, conf['output_dir'] + '/model' + str(itr))
+                if (itr) % conf['save_interval'] == 2:
+                    tf.logging.info('Saving model to' + conf['output_dir'])
+                    saving_saver.save(sess, conf['output_dir'] + '/model' + str(itr))
 
 
-        if itr % 50 == 1:
-            hours = (datetime.now() -starttime).seconds/3600
-            tf.logging.info('running for {0}d, {1}h, {2}min'.format(
-                (datetime.now() - starttime).days,
-                hours,+
-                (datetime.now() - starttime).seconds/60 - hours*60))
-            avg_t_iter = np.sum(np.asarray(t_iter))/len(t_iter)
-            tf.logging.info('time per iteration: {0}'.format(avg_t_iter/1e6))
-            tf.logging.info('expected for complete training: {0}h '.format(avg_t_iter /1e6/3600 * conf['num_iterations']))
+                if itr % 50 == 1:
+                    hours = (datetime.now() -starttime).seconds/3600
+                    tf.logging.info('running for {0}d, {1}h, {2}min'.format(
+                        (datetime.now() - starttime).days,
+                        hours,+
+                        (datetime.now() - starttime).seconds/60 - hours*60))
+                    avg_t_iter = np.sum(np.asarray(t_iter))/len(t_iter)
+                    tf.logging.info('time per iteration: {0}'.format(avg_t_iter/1e6))
+                    tf.logging.info('expected for complete training: {0}h '.format(avg_t_iter /1e6/3600 * conf['num_iterations']))
 
-        if (itr) % SUMMARY_INTERVAL == 2:
-            summary_writer.add_summary(summary_str, itr)
+                if (itr) % SUMMARY_INTERVAL == 2:
+                    summary_writer.add_summary(summary_str, itr)
 
-    sess.close()
-    tf.reset_default_graph()
-    return np.array(t_iter)/1e6
+            sess.close()
+            tf.reset_default_graph()
+            return np.array(t_iter)/1e6
 
 def load_checkpoint(conf, sess, saver, model_file=None):
     """
