a3a1ccab56295f99b9bb270a079b4376f27b3b0f
febert <frederik.ebert@mytum.de>
2018-04-30 17:15:33 -0700

    sc

--- python_visual_mpc/video_prediction/online_training/replay_buffer.py
+++ python_visual_mpc/video_prediction/online_training/replay_buffer.py
@@ -70,5 +70,6 @@ class ReplayBuffer(object):
                 returning_collector = self.data_collectors[info['collector_id']]
                 if info['itraj'] < info['maxtraj']:
                     self.todo_ids.append(returning_collector.run_traj.remote())
+                    self.logger.log('restarting {}'.format(info['collector_id']))
                 else:
                     self.logger.log('tasks finished, no new task launched')
