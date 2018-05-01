1a2740342b7113947ce258b58e46097ac2569e6d
febert <frederik.ebert@mytum.de>
2018-05-01 16:53:54 -0700

    sc

--- experiments/cem_exp/onpolicy/firsttest/trainvid_conf.py
+++ experiments/cem_exp/onpolicy/firsttest/trainvid_conf.py
@@ -25,7 +25,7 @@ config = {
 'num_iterations':200000,
 'output_dir': OUT_DIR,      #'directory for model checkpoints.' ,
 'current_dir': current_dir, #'directory for writing summary.' ,
-'pretrained':base_dir + '/tensorflow_data/sim/onpolicy/updown_sact_onpolonly/modeldata/model196002',     # 'filepath of a pretrained model to resume training from.' ,
+'pretrained_model':base_dir + '/tensorflow_data/sim/onpolicy/updown_sact_onpolonly/modeldata/model196002',     # 'filepath of a pretrained model to resume training from.' ,
 'sequence_length': 30, # 'sequence length to load, including context frames.' ,
 'use_len': 15,
 'skip_frame': 1,            # 'use ever i-th frame to increase prediction horizon' ,
