f0628921611b6530b9c7341b19f59c0335c87692
febert <frederik.ebert@mytum.de>
2018-05-11 16:42:27 -0700

    sc

--- docker/start_docker_mjc.sh
+++ docker/start_docker_mjc.sh
@@ -2,6 +2,7 @@
 nvidia-docker run  -v $VMPC_DATA_DIR:/workspace/pushing_data \
                    -v /home/$USER/Documents/visual_mpc:/mount/visual_mpc \
                    -v /home/$USER/Documents/cloned_projects/video_prediction:/mount/video_prediction \
+                   -v /home/$USER/Documents/cloned_projects/timewarp_prediction:/mount/timewarp_prediction \
 -e VMPC_DATA_DIR=/workspace/pushing_data \
 -it \
 febert/tf_mj1.5:runmount \
