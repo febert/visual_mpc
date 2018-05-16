cca80d319e28670ac0e1eeb12188b52645244bb7
febert <frederik.ebert@mytum.de>
2018-05-16 01:21:37 -0700

    fixed bug in replay buffer

--- experiments/cem_exp/benchmarks/multiobj_pushing/random_ordering/conf.py
+++ experiments/cem_exp/benchmarks/multiobj_pushing/random_ordering/conf.py
@@ -18,6 +18,7 @@ configuration = {
 'setup_predictor':setup_predictor,
 'output_dir': OUT_DIR,      #'directory for model checkpoints.' ,
 'current_dir': current_dir, #'directory for writing summary.' ,
+'pretrained_model':base_dir + '/tensorflow_data/sim/pos_ctrl_updown_sact/modeldata/model152002',     # 'filepath of a pretrained model to resume training from.' ,
 'sequence_length': 15,      # 'sequence length to load, including context frames.' ,
 'skip_frame': 1,            # 'use ever i-th frame to increase prediction horizon' ,
 'context_frames': 2,        # of frames before predictions.' ,
@@ -25,11 +26,15 @@ configuration = {
 'model': 'appflow',            #'model architecture to use - CDNA, DNA, or STP' ,
 'num_transformed_images': 1,   # 'number of masks, usually 1 for DNA, 10 for CDNA, STN.' ,
 'schedsamp_k': -1,      # 'The k hyperparameter for scheduled sampling -1 for no scheduled sampling.' ,
-'batch_size': 10,#############################200,           #'batch size for training' ,
+'train_val_split': 0.95,    #'The percentage of files to use for the training set vs. the validation set.' ,
+'batch_size': 200,           #'batch size for training' ,
+'learning_rate': 0.001,     #'the base learning rate of the generator' ,
 'visualize': '',            #'load model from which to generate visualizations
 'file_visual': '',          # datafile used for making visualizations
 'kern_size': 17,            # size of DNA kerns
+'single_view':"",
 '1stimg_bckgd':'',
+# 'visual_flowvec':'',
 'adim':3,
 'sdim':6,
 'normalization':'in',
@@ -37,7 +42,6 @@ configuration = {
 'orig_size':[48,64],
 'img_height':48,
 'img_width':64,
-'ndesig':1,
-'use_vel':'',                # add the velocity to the state input
-'pretrained_model': base_dir + '/tensorflow_data/sim/onpolicy/updown_sact_onpolonly/modeldata/model196002',
+'ndesig':2,
+'use_vel':''                # add the velocity to the state input
 }
