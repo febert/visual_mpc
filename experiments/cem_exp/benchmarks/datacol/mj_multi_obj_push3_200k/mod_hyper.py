ac7dfcfc05e033b8a48395b81661412b7876c0c9
febert <frederik.ebert@mytum.de>
2018-05-16 17:14:11 -0700

    sc

--- experiments/cem_exp/benchmarks/datacol/mj_multi_obj_push3_200k/mod_hyper.py
+++ experiments/cem_exp/benchmarks/datacol/mj_multi_obj_push3_200k/mod_hyper.py
@@ -15,7 +15,7 @@ folder_name = '/'.join(str.split(__file__, '/')[-2:-1])
 
 agent = {
     'type': AgentMuJoCo,
-    'T': 2,##############75,
+    'T': 75,
     'substeps':50,
     # 'make_final_gif':'',
     'adim':3,
@@ -60,7 +60,7 @@ policy = {
     'rew_all_steps':"",
     'finalweight':10,
     'no_action_bound':"",
-    'num_samples': 10, ###################100,
+    'num_samples': 100,
     'replan_interval':10,
 }
 
