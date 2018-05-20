cce1976b725d37551a4e2c479bb9abbabd776b8d
Sudeep Dasari <sdasari@berkeley.edu>
2018-05-20 10:31:17 -0700

    new grasping params

--- python_visual_mpc/visual_mpc_core/infrastructure/utility/tfrecord_from_file.py
+++ python_visual_mpc/visual_mpc_core/infrastructure/utility/tfrecord_from_file.py
@@ -10,7 +10,7 @@ import cv2
 from python_visual_mpc.visual_mpc_core.infrastructure.utility.save_tf_record import save_tf_record
 class LoadTraj:
     def __init__(self):
-        self.actions, self.X_Xdot_full, self.images = None, None, None
+        self.actions, self.X_Xdot_full, self.images, self.goal_image = None, None, None, None
 def main():
     parser = argparse.ArgumentParser(description='run convert from directory to tf record')
     parser.add_argument('experiment', type=str, help='experiment name')
@@ -45,14 +45,16 @@ def main():
     print('saving to', out_dir)
 
     good_traj_list, bad_traj_list = [], []
-    num_good_saved, num_bad_saved = 0, 0
+    num_good_saved, num_bad_saved = 352, 273
     good_lift_ctr, total_ctr = 0, 0
     traj_group_dirs = glob.glob(data_dir+'/*')
+    
+    agent_config['goal_image'] = True 
     
     for g in traj_group_dirs:
         trajs = glob.glob(g + '/*')
         for t in trajs:
-            if len(glob.glob(t + '/images0/*.png')) != T or len(glob.glob(t + '/images1/*.png')) != T or not os.path.exists(t + '/state_action.pkl'):
+            if len(glob.glob(t + '/images/*.png')) != T or not os.path.exists(t + '/state_action.pkl'):
                 print('TRAJ', t, 'is broken')
                 continue
             try:
@@ -66,37 +68,41 @@ def main():
                 loaded_traj = LoadTraj()
                 goal_pos = state_action['obj_start_end_pos'][1]
                 loaded_traj.actions = state_action['actions']
-                loaded_traj.X_Xdot_full = np.hstack((state_action['qpos'], state_action['qvel'])) #state_action['target_qpos'][:T, :] # # 
-                loaded_traj.images = np.zeros((T, 2, img_height, img_width, 3), dtype = 'uint8')
+                loaded_traj.X_Xdot_full = state_action['target_qpos'][:T, :]# np.hstack((state_action['qpos'], state_action['qvel']))  
+                loaded_traj.images = np.zeros((T, img_height, img_width, 3), dtype = 'uint8')
 
                 good_lift = False
                 total_ctr += 1
-                # if np.sum(np.abs(goal_pos)) > 0:
-                #     good_lift = True
-                #     good_lift_ctr += 1
+                if np.sum(np.abs(goal_pos)) > 0:
+                    good_lift = True
+                    good_lift_ctr += 1
 
-                object_poses = state_action['object_full_pose']
-                lift_mask = np.sum(object_poses[2:, :, 2] >= 0.12, axis = 0).astype(np.bool)
-                if np.any(lift_mask):
-                    if any(np.amax(object_poses[2:,lift_mask,2], axis = 1) - np.amin(object_poses[2:,lift_mask,2], axis=1) >= 0.05):
-                        good_lift = True
-                        good_lift_ctr += 1
-                        print('traj', t, 'is good!')
+                #object_poses = state_action['object_full_pose']
+                #lift_mask = np.sum(object_poses[2:, :, 2] >= 0.12, axis = 0).astype(np.bool)
+                #if np.any(lift_mask):
+                #    if any(np.amax(object_poses[2:,lift_mask,2], axis = 1) - np.amin(object_poses[2:,lift_mask,2], axis=1) >= 0.05):
+                #        good_lift = True
+                #        good_lift_ctr += 1
+                #        print('traj', t, 'is good!')
                 
                 for i in range(T):
-                    img = cv2.imread(t + '/images0/im{}.png'.format(i))[:, :, ::-1]
-                    loaded_traj.images[i, 0] = img
+                    img = cv2.imread(t + '/images/im{}.png'.format(i))[:, :, ::-1]
+                    loaded_traj.images[i] = img
 
-                    img2 = cv2.imread(t + '/images1/im{}.png'.format(i))[:, :, ::-1]
-                    loaded_traj.images[i, 1] = img2
+                    #img2 = cv2.imread(t + '/images1/im{}.png'.format(i))[:, :, ::-1]
+                    #loaded_traj.images[i, 1] = img2
 
-                #     if np.sum(np.abs(goal_pos)) > 0 and all(goal_pos == loaded_traj.X_Xdot_full[i, :2]) and loaded_traj.X_Xdot_full[i, 2] ==-0.08 and loaded_traj.goal_image is None:
-                #         loaded_traj.goal_image = img
-                
-                # if loaded_traj.goal_image is None:
-                #     print('NO GOAL PROBABLY DIDNT LIFT')
-                #     loaded_traj.goal_image = cv2.imread(t + '/images/im{}.png'.format(0))[:, :, ::-1]
-                #     no_lift += 1
+                    if good_lift and all(np.isclose(goal_pos, loaded_traj.X_Xdot_full[i, :2], atol=1e-3)) and loaded_traj.X_Xdot_full[i, 2] ==-0.08 and loaded_traj.goal_image is None:
+                        loaded_traj.goal_image = img
+    
+                if loaded_traj.goal_image is None:
+                    loaded_traj.goal_image = cv2.imread(t + '/images/im{}.png'.format(0))[:, :, ::-1]
+                    
+                    if good_lift:
+                        print('NO DROP')
+                        good_lift_ctr -=1
+                        good_lift = False
+                    
                 if good_lift:
                     good_traj_list.append(loaded_traj)
                 else:
