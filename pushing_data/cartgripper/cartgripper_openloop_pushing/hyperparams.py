c35a8937151bc501a026dcf4a6340272f71407fc
Sudeep Dasari <sdasari@berkeley.edu>
2018-05-26 23:47:16 -0700

    new openloop parameters/pushing code

--- pushing_data/cartgripper/cartgripper_openloop_pushing/hyperparams.py
+++ pushing_data/cartgripper/cartgripper_openloop_pushing/hyperparams.py
@@ -4,7 +4,7 @@ import os.path
 
 import numpy as np
 
-from python_visual_mpc.visual_mpc_core.algorithm.random_policy import RandomPickPolicy
+from python_visual_mpc.visual_mpc_core.algorithm.random_policy import Randompolicy
 from python_visual_mpc.visual_mpc_core.agent.agent_mjc import AgentMuJoCo
 from python_visual_mpc.visual_mpc_core.infrastructure.utility.tfrecord_from_file import DefaultTraj
 IMAGE_WIDTH = 64
@@ -42,7 +42,7 @@ def convert_to_record(state_action):
 
 agent = {
     'type': AgentMuJoCo,
-    'data_save_dir': '/result', #BASE_DIR + '/train',
+    'data_save_dir': BASE_DIR + '/train', #'result/' #BASE_DIR + '/59903/data/train',
     'filename': DATA_DIR+'/mjc_models/cartgripper_grasp.xml',
     'filename_nomarkers': DATA_DIR+'/mjc_models/cartgripper_grasp.xml',
     'not_use_images':"",
@@ -73,25 +73,23 @@ agent = {
     'min_z_lift':0.05,
     'make_final_gif':'', #keep this key in if you want final gif to be created
     'record': BASE_DIR + '/record/',
-    'targetpos_clip':[[-0.5, -0.5, -0.08, -2 * np.pi, -1], [0.5, 0.5, 0.15, 2 * np.pi, 1]],
+    'targetpos_clip':[[-0.5, -0.5, -0.08, -2 * np.pi, -1], [0.5, 0.5, 0., 2 * np.pi, 1]],
     'mode_rel':np.array([True, True, True, True, False]),
     'discrete_gripper' : -1, #discretized gripper dimension,
-    'lift_rejection_sample' : 30,
     'close_once_actions' : True,
-    'object_mass' : 0.05,
-    'master_datadir' : '/raid/ngc2/grasping_data/cartgripper_openloop_randgrasp/'
-#    'file_to_record' : convert_to_record
+    'file_to_record' : convert_to_record,
+    'object_mass' : 0.05
     #'object_meshes':['giraffe'] #folder to original object + convex approximation
     # 'displacement_threshold':0.1,
 }
 
 policy = {
-    'type' : RandomPickPolicy,
+    'type' : Randompolicy,
     'nactions' : 5,
     'repeats' : 3,
     'no_action_bound' : False, 
-    'initial_std': 0.02,   #std dev. in xy
-    'initial_std_lift': 0.01,   #std dev. in xy
+    'initial_std': 0.1,   #std dev. in xy
+    'initial_std_lift': 0.01,   #std dev. in z
     'initial_std_rot' : np.pi / 36,
     'initial_std_grasp' : 2 
 }
