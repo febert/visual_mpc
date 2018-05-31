7be84a9ba1e4fbd42a30c2616a6d763743af7b4f
Sudeep Dasari <sdasari@berkeley.edu>
2018-05-31 10:54:21 -0700

    got 3d calib finished

--- python_visual_mpc/sawyer/util/calib_cam0.py
+++ python_visual_mpc/sawyer/util/calib_cam0.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from cvxpy import *
 
@@ -7,7 +6,7 @@ Script used to find H and t for the transformation
 x_Sawyer = H x_fcam + t
 """
 
-# The ten calibration points in the fcam coordinate system
+# The 17 calibration points in the fcam coordinate system
 p_1_fcam = np.array([0.14712389, 0.09905503, 0.79387856]).reshape((3, 1))
 p_2_fcam = np.array([-0.14546939, 0.18702181, 0.67870914]).reshape((3, 1))
 p_3_fcam = np.array([-0.15560695, 0.08235386, 0.85817415]).reshape((3, 1))
@@ -18,10 +17,19 @@ p_7_fcam = np.array([-0.22354235, 0.00811434, 0.92185106]).reshape((3, 1))
 p_8_fcam = np.array([-0.0294278, 0.20265216, 0.64906355]).reshape((3, 1))
 p_9_fcam = np.array([0.05585613, 0.04350206, 0.87090842]).reshape((3, 1))
 p_10_fcam = np.array([0.11689826, 0.00371152, 0.89444079]).reshape((3, 1))
+p_11_fcam = np.array([-0.02438138, 0.1851034, 0.6782539]).reshape((3, 1))
+p_12_fcam = np.array([-0.15156119, -0.10647377, 0.73095551]).reshape((3, 1))
+p_13_fcam = np.array([0.02885072, -0.00685847, 0.59970956]).reshape((3, 1))
+p_14_fcam = np.array([-0.22566872, 0.12646914, 0.62731728]).reshape((3, 1))
+p_15_fcam = np.array([-0.20807963, -0.03747736, 0.81478915]).reshape((3, 1))
+p_16_fcam = np.array([0.04447122, 0.01679777, 0.57775683]).reshape((3, 1))
+p_17_fcam = np.array([0.16024029, -0.04023288, 0.87650725]).reshape((3, 1))
 
-p_fcam = [p_1_fcam, p_2_fcam, p_3_fcam, p_4_fcam, p_5_fcam, p_6_fcam, p_7_fcam, p_8_fcam, p_9_fcam, p_10_fcam]
+p_fcam = [p_1_fcam, p_2_fcam, p_3_fcam, p_4_fcam, p_5_fcam, p_6_fcam, p_7_fcam, 
+          p_8_fcam, p_9_fcam, p_10_fcam, p_11_fcam, p_12_fcam, p_13_fcam, p_14_fcam,
+          p_15_fcam, p_16_fcam, p_17_fcam]
 
-# The ten corresponding calibration points in the Sawyer coordinate system
+# The 17 corresponding calibration points in the Sawyer coordinate system
 p_1_sawyer = np.array([0.62404614, 0.15892005, 0.18217241]).reshape((3, 1))
 p_2_sawyer = np.array([0.77895433, -0.09286761, 0.18451509]).reshape((3, 1))
 p_3_sawyer = np.array([0.6372649, -0.08373514, 0.18383977]).reshape((3, 1))
@@ -32,9 +40,18 @@ p_7_sawyer = np.array([0.49541386, -0.17370474, 0.1830696]).reshape((3, 1))
 p_8_sawyer = np.array([0.81347582, 0.00699111, 0.18454398]).reshape((3, 1))
 p_9_sawyer = np.array([0.5533918, 0.09036484, 0.18636769]).reshape((3, 1))
 p_10_sawyer = np.array([0.48335695, 0.13941328, 0.18298395]).reshape((3, 1))
+p_11_sawyer = np.array([0.77663933, 0.00884187, 0.17993153]).reshape((3, 1))
+p_12_sawyer = np.array([0.56730004, -0.11262669, 0.35859245]).reshape((3, 1))
+p_13_sawyer = np.array([0.72347009, 0.04582043, 0.35488332]).reshape((3, 1))
+p_14_sawyer = np.array([0.79464528, -0.1679792, 0.25699683]).reshape((3, 1))
+p_15_sawyer = np.array([0.55278069, -0.16831283, 0.26525751]).reshape((3, 1))
+p_16_sawyer = np.array([0.73617149, 0.06522711, 0.35470406]).reshape((3, 1))
+p_17_sawyer = np.array([0.49645224, 0.17386133, 0.23895355]).reshape((3, 1))
 
 
-p_sawyer = [p_1_sawyer, p_2_sawyer, p_3_sawyer, p_4_sawyer, p_5_sawyer, p_6_sawyer, p_7_sawyer, p_8_sawyer, p_9_sawyer, p_10_sawyer]
+p_sawyer = [p_1_sawyer, p_2_sawyer, p_3_sawyer, p_4_sawyer, p_5_sawyer, p_6_sawyer, p_7_sawyer, 
+            p_8_sawyer, p_9_sawyer, p_10_sawyer, p_11_sawyer, p_12_sawyer, p_13_sawyer, p_14_sawyer,
+            p_15_sawyer,p_16_sawyer,p_17_sawyer]
 
 # Optimization variables
 H = Variable(3, 3)
--- python_visual_mpc/sawyer/util/calib_cam1.py
+++ python_visual_mpc/sawyer/util/calib_cam1.py
@@ -18,10 +18,19 @@ p_7_lcam = np.array([-0.11376011, 0.19173899, 0.55449037]).reshape((3, 1))
 p_8_lcam = np.array([0.24237161, 0.06557262, 0.69774114]).reshape((3, 1))
 p_9_lcam = np.array([-0.05169478, 0.03228137, 0.73730863]).reshape((3, 1))
 p_10_lcam = np.array([-0.12158303, -0.00191091, 0.71539879]).reshape((3, 1))
+p_11_lcam = np.array([0.20548082, 0.06576004, 0.69871352]).reshape((3, 1))
+p_12_lcam = np.array([-0.04660003, -0.00742236, 0.4811249]).reshape((3, 1))
+p_13_lcam = np.array([0.12736719, -0.11713373, 0.60629696]).reshape((3, 1))
+p_14_lcam = np.array([0.21898202, 0.10598259, 0.49980709]).reshape((3, 1))
+p_15_lcam = np.array([-0.06648953, 0.10429062, 0.47103609]).reshape((3, 1))
+p_16_lcam = np.array([0.16626607, -0.12558073, 0.61680587]).reshape((3, 1))
+p_17_lcam = np.array([-0.1053655, -0.07381605, 0.75874501]).reshape((3, 1))
 
-p_lcam = [p_1_lcam, p_2_lcam, p_3_lcam, p_4_lcam, p_5_lcam, p_6_lcam, p_7_lcam, p_8_lcam, p_9_lcam, p_10_lcam]
+p_lcam = [p_1_lcam, p_2_lcam, p_3_lcam, p_4_lcam, p_5_lcam, p_6_lcam, p_7_lcam, 
+          p_8_lcam, p_9_lcam, p_10_lcam, p_11_lcam, p_12_lcam, p_13_lcam, p_14_lcam,
+          p_15_lcam, p_16_lcam, p_17_lcam]
 
-# The ten corresponding calibration points in the Sawyer coordinate system
+# The 17 corresponding calibration points in the Sawyer coordinate system
 p_1_sawyer = np.array([0.62404614, 0.15892005, 0.18217241]).reshape((3, 1))
 p_2_sawyer = np.array([0.77895433, -0.09286761, 0.18451509]).reshape((3, 1))
 p_3_sawyer = np.array([0.6372649, -0.08373514, 0.18383977]).reshape((3, 1))
@@ -32,9 +41,18 @@ p_7_sawyer = np.array([0.49541386, -0.17370474, 0.1830696]).reshape((3, 1))
 p_8_sawyer = np.array([0.81347582, 0.00699111, 0.18454398]).reshape((3, 1))
 p_9_sawyer = np.array([0.5533918, 0.09036484, 0.18636769]).reshape((3, 1))
 p_10_sawyer = np.array([0.48335695, 0.13941328, 0.18298395]).reshape((3, 1))
+p_11_sawyer = np.array([0.77663933, 0.00884187, 0.17993153]).reshape((3, 1))
+p_12_sawyer = np.array([0.56730004, -0.11262669, 0.35859245]).reshape((3, 1))
+p_13_sawyer = np.array([0.72347009, 0.04582043, 0.35488332]).reshape((3, 1))
+p_14_sawyer = np.array([0.79464528, -0.1679792, 0.25699683]).reshape((3, 1))
+p_15_sawyer = np.array([0.55278069, -0.16831283, 0.26525751]).reshape((3, 1))
+p_16_sawyer = np.array([0.73617149, 0.06522711, 0.35470406]).reshape((3, 1))
+p_17_sawyer = np.array([0.49645224, 0.17386133, 0.23895355]).reshape((3, 1))
 
 
-p_sawyer = [p_1_sawyer, p_2_sawyer, p_3_sawyer, p_4_sawyer, p_5_sawyer, p_6_sawyer, p_7_sawyer, p_8_sawyer, p_9_sawyer, p_10_sawyer]
+p_sawyer = [p_1_sawyer, p_2_sawyer, p_3_sawyer, p_4_sawyer, p_5_sawyer, p_6_sawyer, p_7_sawyer, 
+            p_8_sawyer, p_9_sawyer, p_10_sawyer, p_11_sawyer, p_12_sawyer, p_13_sawyer, p_14_sawyer,
+            p_15_sawyer,p_16_sawyer,p_17_sawyer]
 
 # Optimization variables
 H = Variable(3, 3)
