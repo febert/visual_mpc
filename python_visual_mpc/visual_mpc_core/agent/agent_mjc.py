586d8917963dd7e964a9408fc6f1e8bfee5195c3
Sudeep Dasari <sdasari@berkeley.edu>
2018-06-01 16:31:06 -0700

    sc

--- python_visual_mpc/visual_mpc_core/agent/agent_mjc.py
+++ python_visual_mpc/visual_mpc_core/agent/agent_mjc.py
@@ -291,10 +291,6 @@ class AgentMuJoCo(object):
                 self.target_qpos, self.t_down, self.gripper_up, self.gripper_closed = get_target_qpos(
                     self.target_qpos, self._hyperparams, mj_U, t, self.gripper_up, self.gripper_closed, self.t_down, traj.X_full[t,2])
                 traj.target_qpos[t + 1] = self.target_qpos.copy()
-<<<<<<< HEAD
-
-=======
->>>>>>> dev
             else:
                 ctrl = mj_U.copy()
 
