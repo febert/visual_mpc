6b4c68e43e3d866723f97db62fc45b9c9285768a
febert <frederik.ebert@mytum.de>
2018-06-10 14:27:16 -0700

    sc

--- experiments/cem_exp/benchmarks/alexmodel/savp_2cam_registrationtradeoff/mod_hyper.py
+++ experiments/cem_exp/benchmarks/alexmodel/savp_2cam_registrationtradeoff/mod_hyper.py
@@ -12,7 +12,7 @@ from python_visual_mpc.visual_mpc_core.agent.agent_mjc import AgentMuJoCo
 import numpy as np
 agent = {
     'type': AgentMuJoCo,
-    'T': 3,
+    'T': 30,
     'substeps':200,
     'adim':3,
     'sdim':6,
--- python_visual_mpc/sawyer/visual_mpc_rospkg/src/utils/annotate_benchmark.py
+++ python_visual_mpc/sawyer/visual_mpc_rospkg/src/utils/annotate_benchmark.py
@@ -32,26 +32,75 @@ def annotate(exp_dir):
         final_pos = np.round(c.coords).astype(np.int)
 
         final_dist = np.linalg.norm(goal_pix - final_pos)
-        improvement = np.linalg.norm(desig_pix_t0 - goal_pix) - final_dist
+        initial_dist = np.linalg.norm(desig_pix_t0 - goal_pix)
+        improvement = initial_dist - final_dist
         scores.append(final_dist)
         improvements.append(improvement)
 
+        ann_stats = {'initial_dist': initial_dist, 'improvments':improvements, 'scores':scores}
+        pkl.dump(ann_stats, open(exp_dir + '/ann_stats.pkl','wb'))
+
+    write(exp_dir, ann_stats)
+
+def write(exp_dir, stat):
+    improvement = stat['improvement']
+
+    scores = stat['scores']
+    if 'initial_dist' in stat:
+        initial_dist = stat['initial_dist']
+    else:
+        initial_dist = None
+    integrated_poscost = stat['integrated_poscost']
+    term_t = stat['term_t']
+
+    sorted_ind = improvement.argsort()[::-1]
+
+    mean_imp = np.mean(improvement)
+    med_imp = np.median(improvement)
+    mean_dist = np.mean(scores)
+    med_dist = np.median(scores)
+    mean_integrated_poscost = np.mean(integrated_poscost)
+    med_integrated_poscost = np.median(integrated_poscost)
+
+    lifted = stat['lifted'].astype(np.int)
+
+    result_file = exp_dir + '/result.'
+    f = open(result_file, 'w')
+    f.write('---\n')
+    f.write('overall best pos improvement: {0} of traj {1}\n'.format(improvement[sorted_ind[0]], sorted_ind[0]))
+    f.write('overall worst pos improvement: {0} of traj {1}\n'.format(improvement[sorted_ind[-1]], sorted_ind[-1]))
+    f.write('average pos improvemnt: {0}\n'.format(mean_imp))
+    f.write('median pos improvement {}'.format(med_imp))
+    f.write('standard deviation of population {0}\n'.format(np.std(improvement)))
+    f.write('standard error of the mean (SEM) {0}\n'.format(np.std(improvement) / np.sqrt(improvement.shape[0])))
+    f.write('---\n')
+    f.write('average pos score: {0}\n'.format(mean_dist))
+    f.write('median pos score {}'.format(med_dist))
+    f.write('standard deviation of population {0}\n'.format(np.std(scores)))
+    f.write('standard error of the mean (SEM) {0}\n'.format(np.std(scores) / np.sqrt(scores.shape[0])))
+    f.write('---\n')
+    f.write('mean imp, med imp, mean dist, med dist {}, {}, {}, {}\n'.format(mean_imp, med_imp, mean_dist, med_dist))
+    f.write('---\n')
+    if initial_dist is not None:
+        f.write('average initial dist: {0}\n'.format(np.mean(initial_dist)))
+        f.write('median initial dist: {0}\n'.format(np.median(initial_dist)))
+        f.write('----------------------\n')
+    if 'ztarget' in conf:
+        f.write('traj: improv, score, lifted at end, rank\n')
+        f.write('----------------------\n')
+
+        for n, t in enumerate(range(conf['start_index'], i_traj)):
+            f.write('{}: {}, {}, {}, :{}\n'.format(t, improvement[n], scores[n], improvement[n] > 0.05,
+                                                   np.where(sorted_ind == n)[0][0]))
+    else:
+        f.write('traj: improv, score, term_t, lifted, rank\n')
+        f.write('----------------------\n')
+        for n, t in enumerate(range(conf['start_index'], i_traj)):
+            f.write('{}: {}, {}, {}, {}:{}\n'.format(t, improvement[n], scores[n], term_t[n], lifted[n],
+                                                     np.where(sorted_ind == n)[0][0]))
+    f.close()
 
 
-
-
-
-
-
-
-def read_data(exp_dir)
-
-    for ex in exmps:
-        print(ex)
-        c = Getdesig(goal_images[ex])
-        goal_pix.append(np.round(c.coords).astype(np.int))
-        # goal_pix.append(np.array([0,0]))
-        annotated_goal_images.append(add_crosshairs_single(goal_images[ex], goal_pix[-1]))
 
 
 def cal
