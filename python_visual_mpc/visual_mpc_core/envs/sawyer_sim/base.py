e5aeb0f43e2b8634274f4051de490af027d2bd80
Sudeep Dasari <sdasari@berkeley.edu>
2018-07-06 22:55:54 -0700

    added test env

--- python_visual_mpc/visual_mpc_core/envs/sawyer_sim/base.py
+++ python_visual_mpc/visual_mpc_core/envs/sawyer_sim/base.py
@@ -5,8 +5,8 @@ import cv2
 BASE_DIR = '/'.join(str.split(python_visual_mpc.__file__, '/')[:-2])
 asset_base_path = BASE_DIR + '/mjc_models/sawyer_assets/sawyer_xyz/'
 
-class SawyerEnv(BaseMujocoEnv):
-    def __init__(self):
+class BaseSawyerEnv(BaseMujocoEnv):
+    def __init__(self, throw):
         super().__init__(asset_base_path + 'sawyer_push_puck.xml')
         dual_render = self.render()
 
