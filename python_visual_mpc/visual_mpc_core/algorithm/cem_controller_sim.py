b5dc107b7c45e0eec304b83f48e8460b99342e78
febert <frederik.ebert@mytum.de>
2018-08-21 14:06:01 -0700

    adapt cemcontroller sim

--- python_visual_mpc/visual_mpc_core/algorithm/cem_controller_sim.py
+++ python_visual_mpc/visual_mpc_core/algorithm/cem_controller_sim.py
@@ -24,6 +24,7 @@ from python_visual_mpc.visual_mpc_core.agent.agent_mjc import get_target_qpos
 
 from .cem_controller_base import CEM_Controller_Base
 
+from python_visual_mpc.visual_mpc_core.agent.general_agent import resize_store
 
 class CEM_Controller_Sim(CEM_Controller_Base):
     """
@@ -37,13 +38,6 @@ class CEM_Controller_Sim(CEM_Controller_Base):
 
     def finish(self):
         self.viewer.finish()
-
-    def setup_mujoco(self):
-        sim_state = self.sim.get_state()
-        sim_state.qpos[:] = self.init_model.data.qpos
-        sim_state.qvel[:] = self.init_model.data.qvel
-        self.sim.set_state(sim_state)
-        self.sim.forward()
 
     def eval_action(self):
         abs_distances = []
@@ -91,7 +85,36 @@ class CEM_Controller_Sim(CEM_Controller_Base):
         return actions_costs
 
 
+    def setup_mujoco(self):
+        env_type, env_params = self.agentparams['env']
+        self.env = env_type(env_params, self._reset_state)
+
     def get_rollouts(self, actions, cem_itr, itr_times):
+        all_scores = np.empty(self.M, dtype=np.float64)
+        image_list = []
+        for smp in range(self.M):
+            self.setup_mujoco()
+            score, images = self.sim_rollout(actions[smp])
+            image_list.append(images)
+            # print('score', score)
+            per_time_multiplier = np.ones([len(score)])
+            per_time_multiplier[-1] = self.policyparams['finalweight']
+            all_scores[smp] = np.sum(per_time_multiplier*score)
+
+            # if smp % 10 == 0 and self.verbose:
+            #     self.save_gif(images, '_{}'.format(smp))
+
+        if self.verbose:
+            bestindices = all_scores.argsort()[:self.K]
+            best_vids = [image_list[ind] for ind in bestindices]
+            vid = [np.concatenate([b[t_] for b in best_vids], 1) for t_ in range(self.naction_steps * self.repeat)]
+            self.save_gif(vid, 't{}_iter{}'.format(self.t, cem_itr))
+
+        return all_scores
+
+
+
+    def old_get_rollouts(self, actions, cem_itr, itr_times):
         all_scores = np.empty(self.M, dtype=np.float64)
         image_list = []
         for smp in range(self.M):
@@ -118,7 +141,124 @@ class CEM_Controller_Sim(CEM_Controller_Base):
         file_path = self.agentparams['record']
         npy_to_gif(images, file_path +'/video'+name)
 
+
+    def _post_process_obs(self, env_obs, agent_data, initial_obs=False):
+        """
+        Copied from general_agent.py !!
+
+        Handles conversion from the environment observations, to agent observation
+        space. Observations are accumulated over time, and images are resized to match
+        the given image_heightximage_width dimensions.
+
+        Original images from cam index 0 are added to buffer for saving gifs (if needed)
+
+        Data accumlated over time is cached into an observation dict and returned. Data specific to each
+        time-step is returned in agent_data
+
+        :param env_obs: observations dictionary returned from the environment
+        :param initial_obs: Whether or not this is the first observation in rollout
+        :return: obs: dictionary of observations up until (and including) current timestep
+        """
+        agent_img_height = self._hyperparams['image_height']
+        agent_img_width = self._hyperparams['image_width']
+
+        if initial_obs:
+            T = self._hyperparams['T'] + 1
+            self._agent_cache = {}
+            for k in env_obs:
+                if k == 'images':
+                    if 'obj_image_locations' in env_obs:
+                        self.traj_points = []
+                    n_cams = env_obs['images'].shape[0]
+                    self._agent_cache['images'] = np.zeros((T, n_cams, agent_img_height, agent_img_width, 3),
+                                                           dtype = np.uint8)
+                elif isinstance(env_obs[k], np.ndarray):
+                    obs_shape = [T] + list(env_obs[k].shape)
+                    self._agent_cache[k] = np.zeros(tuple(obs_shape), dtype=env_obs[k].dtype)
+                else:
+                    self._agent_cache[k] = []
+            self._cache_cntr = 0
+
+        t = self._cache_cntr
+        self._cache_cntr += 1
+
+        point_target_width = float(self._hyperparams.get('point_space_width', agent_img_width))
+        obs = {}
+        for k in env_obs:
+            if k == 'images':
+                self.large_images_traj.append(env_obs['images'][0])  #only take first camera
+                resize_store(t, self._agent_cache['images'], env_obs['images'])
+
+            elif k == 'obj_image_locations':
+                self.traj_points.append(copy.deepcopy(env_obs['obj_image_locations'][0]))  #only take first camera
+                env_obs['obj_image_locations'] = np.round((env_obs['obj_image_locations'] *
+                                                           point_target_width / env_obs['images'].shape[2])).astype(np.int64)
+                self._agent_cache['obj_image_locations'][t] = env_obs['obj_image_locations']
+            elif isinstance(env_obs[k], np.ndarray):
+                self._agent_cache[k][t] = env_obs[k]
+            else:
+                self._agent_cache[k].append(env_obs[k])
+            obs[k] = self._agent_cache[k][:self._cache_cntr]
+
+        if 'obj_image_locations' in env_obs:
+            agent_data['desig_pix'] = env_obs['obj_image_locations']
+        if self._goal_image is not None:
+            agent_data['goal_image'] = self._goal_image
+        if self._goal_obj_pose is not None:
+            agent_data['goal_pos'] = self._goal_obj_pose
+            agent_data['goal_pix'] = self.env.get_goal_pix(point_target_width)
+        return obs
+
+
     def sim_rollout(self, actions):
+        """
+                Rolls out policy for T timesteps
+                :param policy: Class extending abstract policy class. Must have act method (see arg passing details)
+                :param i_tr: Rollout attempt index (increment each time trajectory fails rollout)
+                :return: - agent_data: Dictionary of extra statistics/data collected by agent during rollout
+                         - obs: dictionary of environment's observations. Each key maps to that values time-history
+                         - policy_ouputs: list of policy's outputs at each timestep.
+                         Note: tfrecord saving assumes all keys in agent_data/obs/policy_outputs point to np arrays or primitive int/float
+                """
+        self._init()
+        agent_data, policy_outputs = {}, []
+
+        # Take the sample.
+        t = 0
+        done = False
+        initial_env_obs, _ = self.env.reset()
+        obs = self._post_process_obs(initial_env_obs, agent_data, True)
+
+        traj_ok = True
+        while not done:
+            """
+            Every time step send observations to policy, acts in environment, and records observations
+
+            Policy arguments are created by
+                - populating a kwarg dict using get_policy_arg
+                - calling policy.act with given dictionary
+
+            Policy returns an object (pi_t) where pi_t['actions'] is an action that can be fed to environment
+            Environment steps given action and returns an observation
+            """
+
+            try:
+                obs = self._post_process_obs(self.env.step(actions[t]), agent_data)
+            except ValueError:
+                return {'traj_ok': False}, None, None
+
+            self._required_rollout_metadata(agent_data, traj_ok, t)
+            if (self._hyperparams['T'] - 1) == t:
+                done = True
+            t += 1
+
+        if not self.env.valid_rollout():
+            traj_ok = False
+
+        self._required_rollout_metadata(agent_data, traj_ok, t)
+        return agent_data, obs, policy_outputs
+
+    def old_sim_rollout(self, actions):
         costs = []
         self.hf_qpos_l = []
         self.hf_target_qpos_l = []
