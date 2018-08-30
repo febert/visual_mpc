4ceece64e035b91a4254a5d801fb1716423e741e
Sudeep Dasari <sdasari@berkeley.edu>
2018-08-30 13:26:45 -0700

    sc

--- python_visual_mpc/visual_mpc_core/envs/mujoco_env/cartgripper_env/base_cartgripper.py
+++ python_visual_mpc/visual_mpc_core/envs/mujoco_env/cartgripper_env/base_cartgripper.py
@@ -1,5 +1,4 @@
-
-m python_visual_mpc.visual_mpc_core.envs.mujoco_env.base_mujoco_env import BaseMujocoEnv
+from python_visual_mpc.visual_mpc_core.envs.mujoco_env.base_mujoco_env import BaseMujocoEnv
 import numpy as np
 import python_visual_mpc
 from python_visual_mpc.visual_mpc_core.envs.mujoco_env.util.create_xml import create_object_xml, create_root_xml, clean_xml
